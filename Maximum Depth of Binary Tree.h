e5b9fac7c0c8cf06c5ed6a434dcdc0a2b2609825
Shichao Jin <jsc0218@gmail.com>
2013-12-29 21:27:17 +0800

    add iterative version

--- Maximum Depth of Binary Tree.h
+++ Maximum Depth of Binary Tree.h
@@ -14,10 +14,36 @@ The maximum depth is the number of nodes along the longest path from the root no
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ 
+ // Recursive Version
 class Solution {
 public:
     int maxDepth(TreeNode *root) {
         if (!root) return 0;
         return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
+
+// Iterative Version, NULL is a level separator
+class Solution {
+public:
+    int maxDepth(TreeNode *root) {
+        if (!root) return 0;
+        queue<TreeNode *> q;
+        q.push(root);
+        q.push(NULL);
+        int depth = 0;
+        while (!q.empty()) {
+            TreeNode *node = q.front();
+            q.pop();
+            if (!node) {
+                ++depth;
+                if (q.empty()) return depth;
+                q.push(NULL);
+            } else {
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+            }
+        }
     }
 };
