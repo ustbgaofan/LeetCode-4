e64e80eb8bbfdb6114cd807716bf67554be9708b
Shichao Jin <jsc0218@gmail.com>
2014-01-01 15:15:50 +0800

    version 1

--- Two Sum.h
+++ Two Sum.h
@@ -9,22 +9,23 @@ You may assume that each input would have exactly one solution.
 Input: numbers={2, 7, 11, 15}, target=9
 Output: index1=1, index2=2
 */
-bool compare(pair<int,int> p1, pair<int,int> p2) {
-    return p1.second < p2.second;
-}
 
+// Version 1, Sort+BinarySearch, O(nlogn)+O(log n!) = O(nlogn)+O(nlogn)
 class Solution {
 public:
+    static bool compare(pair<int,int> p1, pair<int,int> p2) { 
+        return p1.second < p2.second; 
+    }
+    
     vector<int> twoSum(vector<int> &numbers, int target) {
         vector<pair<int, int>> orderedVec;
         int size = numbers.size();
-        for (int i=0; i<size; ++i) {
+        for (int i=0; i<size; ++i)
             orderedVec.push_back(make_pair(i+1, numbers[i]));
-        }
         sort(orderedVec.begin(), orderedVec.end(), compare);
         vector<int> result;
         for (int i=0; i<size; ++i) {
-            int val = target - orderedVec[i].second, begin = 0, end = size - 1;
+            int val = target - orderedVec[i].second, begin = i + 1, end = size - 1;
             bool bFind = false;
             while (begin <= end) {
                 int mid = (begin + end)/2;
@@ -32,22 +33,10 @@ public:
                     begin = mid + 1;
                 } else if (orderedVec[mid].second > val) {
                     end = mid - 1;
-                } else if (orderedVec[mid].second == val && mid != i) {
-                    result.push_back(orderedVec[i].first);
-                    result.push_back(orderedVec[mid].first);
-                    bFind = true;
-                    break;
-                } else {
-                    if (orderedVec[mid-1].second == val) {
-                        result.push_back(orderedVec[i].first);
-                        result.push_back(orderedVec[mid-1].first);
-                        bFind = true;
-                    } 
-                    if (orderedVec[mid+1].second == val) {
-                        result.push_back(orderedVec[i].first);
-                        result.push_back(orderedVec[mid+1].first);
-                        bFind = true;
-                    } 
+                } else if (orderedVec[mid].second == val) {
+					result.push_back(orderedVec[i].first);
+					result.push_back(orderedVec[mid].first);
+					bFind = true;
                     break;
                 }
             }
