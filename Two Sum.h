8fe5c46075187d1e838af889d81625ffb5ebb3eb
Shichao Jin <jsc0218@gmail.com>
2014-01-02 19:27:09 +0800

    version 3 & 4

--- Two Sum.h
+++ Two Sum.h
@@ -72,3 +72,50 @@ public:
         return result;
     }
 };
+
+// Version 3, hash(unordered_map)
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        unordered_map<int, vector<int>> hash;
+        int size = numbers.size();
+        for (int i=0; i<size; ++i) {
+            hash[numbers[i]].push_back(i+1);
+        }
+        vector<int> result;
+        for (int i=0; i<size; ++i) {
+            int val = target - numbers[i];
+            if (hash.find(val) != hash.end()) {
+                if (val != numbers[i]) {
+                    result.push_back(min(i+1, hash[val][0]));
+                    result.push_back(max(i+1, hash[val][0]));
+                    break;
+                } else if (hash[val].size() >= 2) {
+                    result.push_back(min(hash[val][0], hash[val][1]));
+                    result.push_back(max(hash[val][0], hash[val][1]));
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+// Version 4, hash(unordered_map)+one time insert
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        unordered_map<int, int> hash;
+        int size = numbers.size();
+        vector<int> result;
+        for (int i=0; i<size; ++i) {
+            int val = target - numbers[i];
+            if (hash.find(val) != hash.end()) {
+                result.push_back(min(hash[val], i+1));
+                result.push_back(max(hash[val], i+1));
+                break;
+            } else hash[numbers[i]] = i + 1;
+        }
+        return result;
+    }
+};
