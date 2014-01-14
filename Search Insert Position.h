9eb608535e1d1a16ce8082cac22df619baf832a6
Shichao Jin <jsc0218@gmail.com>
2014-01-14 13:33:26 +0800

    Search Insert Position

---
+++ Search Insert Position.h
@@ -0,0 +1,27 @@
+// Search Insert Position
+/*
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 ¡ú 2
+[1,3,5,6], 2 ¡ú 1
+[1,3,5,6], 7 ¡ú 4
+[1,3,5,6], 0 ¡ú 0
+*/
+
+
+class Solution {
+public:
+    int searchInsert(int A[], int n, int target) {
+        int begin = 0, end = n - 1, mid;
+        while (begin <= end) {
+            mid = (begin + end)/2;
+            if (A[mid] == target) return mid;
+            else if (A[mid] < target) begin = mid + 1;
+            else end = mid - 1;
+        }
+    }
+    return begin;
+};
