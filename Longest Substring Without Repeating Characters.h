aae077947d9f128b4b91d25943f1f7aa295a58a1
Shichao Jin <jsc0218@gmail.com>
2014-02-01 15:45:47 +0800

    refactor

--- Longest Substring Without Repeating Characters.h
+++ Longest Substring Without Repeating Characters.h
@@ -5,14 +5,33 @@ For example, the longest substring without repeating letters for "abcabcbb" is "
 which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
 */
 
+// Version 1, there are unnecessary calculations about max 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         bool visited[256] = {false};
         int maxLen = 0, size = s.size();
-        for (int i=0, j=0; i<size, j<size; ++i) {
+        for (int i=0, j=0; i<size; ++i) {
+            while (j<size && !visited[s[j]]) visited[s[j++]] = true;
+            maxLen = max(j-i, maxLen);  
+            if (j == size) break;
+            visited[s[i]] = false;
+        }
+        return maxLen;
+    }
+};
+
+// Version 2, no unnecessary calculations about max
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        bool visited[256] = {false};
+        int maxLen = 0, size = s.size();
+        for (int i=0, j=0; i<size; ++i) {
             while (j<size && !visited[s[j]]) visited[s[j++]] = true;
             maxLen = max(j-i, maxLen);
+            if (j == size) break;
+            while (s[i] != s[j]) visited[s[i++]] = false;
             visited[s[i]] = false;
         }
         return maxLen;
