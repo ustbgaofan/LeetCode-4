28908cc02ac53796a1c9a0beddbc61d9795400cf
Shichao Jin <jsc0218@gmail.com>
2014-02-01 16:10:30 +0800

    modify initialization

--- Longest Substring Without Repeating Characters.h
+++ Longest Substring Without Repeating Characters.h
@@ -9,7 +9,8 @@ which the length is 3. For "bbbbb" the longest substring is "b", with the length
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        bool visited[256] = {false};
+        bool visited[256];
+		memset(visited, 0, sizeof(visited));
         int maxLen = 0, size = s.size();
         for (int i=0, j=0; i<size; ++i) {
             while (j<size && !visited[s[j]]) visited[s[j++]] = true;
@@ -25,7 +26,8 @@ public:
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        bool visited[256] = {false};
+        bool visited[256];
+		memset(visited, 0, sizeof(visited));
         int maxLen = 0, size = s.size();
         for (int i=0, j=0; i<size; ++i) {
             while (j<size && !visited[s[j]]) visited[s[j++]] = true;
