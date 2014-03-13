5fbaf98bbda4a5b04186ed2f14f826fe835b284f
Shichao Jin <jsc0218@gmail.com>
2014-03-13 11:57:52 +0800

    Flatten Binary Tree to Linked List

---
+++ Flatten Binary Tree to Linked List.h
@@ -0,0 +1,80 @@
+// Flatten Binary Tree to Linked List
+/*
+Given a binary tree, flatten it to a linked list in-place.
+
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+click to show hints.
+
+Hints:
+If you notice carefully in the flattened tree, each node's right child points to the next node of a pre-order traversal.
+*/
+
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+// Recursive Version, time complexity O(n)
+class Solution {
+public:
+    void flatten(TreeNode *root, TreeNode *&next) {
+        if (!root) return;
+        if (!root->left && !root->right) {
+            root->right = next;
+            next = root;
+            return;
+        }
+        flatten(root->right, next);
+        flatten(root->left, next);
+        root->left = NULL;
+        root->right = next;
+        next = root;
+    }
+    
+    void flatten(TreeNode *root) {
+        TreeNode *next = NULL;
+        flatten(root, next);
+    }
+};
+
+// Iterative Version, time complexity O(n)
+class Solution {
+public:
+    void flatten(TreeNode *root) {
+        while (root) {
+            if (root->left) {
+                TreeNode *subtree = root->right;
+                root->right = root->left;
+                root->left = NULL;
+                TreeNode *next = root;
+                while (next->right) next = next->right;
+                next->right = subtree;  // may not be the final place of subtree
+            }
+            root = root->right;
+        }
+    }
+};
