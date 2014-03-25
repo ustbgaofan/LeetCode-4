c263d21a34b7f0fc6742039b1023de1f89563ece
Shichao Jin <jsc0218@gmail.com>
2014-03-25 13:46:36 +0800

    add solution

--- Implement strStr().h
+++ Implement strStr().h
@@ -34,3 +34,26 @@ public:
         }
     }
 };
+
+// KMP algorithm, time complexity O(n)=O(n+m), space complexity O(m)
+class Solution {
+public:
+    char *strStr(char *haystack, char *needle) {
+        int m = strlen(needle);
+        if (m == 0) return haystack;
+	    vector<int> Pi(m, 0);
+	    for (int q=1, k=0; q<m; ++q) {
+		    while (k>0 && needle[k]!=needle[q]) k = Pi[k-1];
+		    if (needle[k] == needle[q]) ++k;
+		    Pi[q] = k;
+	    }
+	
+	    int n = strlen(haystack);
+	    for (int i=0, q=0; i<n; ++i) {
+		    while (q>0 && needle[q] != haystack[i]) q = Pi[q-1];
+		    if (needle[q] == haystack[i]) ++q;
+		    if (q == m) return haystack + i - m + 1;
+	    }
+	    return NULL;
+    }
+};
