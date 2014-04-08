7036dee21c6e1b24de8d5436d0c1ed084376225a
Shichao Jin <jsc0218@gmail.com>
2014-04-08 20:57:26 +0800

    add 3rd version

--- Spiral Matrix II.h
+++ Spiral Matrix II.h
@@ -52,3 +52,22 @@ public:
         return res;
     }
 };
+
+// A Very Clean Solution!
+class Solution {
+public:
+    vector<vector<int> > generateMatrix(int n) {
+        vector<vector<int>> res(n, vector<int>(n));
+        for (int val=0, imin=0, imax=n-1, jmin=0, jmax=n-1; ; ) {
+            for (int j=jmin; j<=jmax; ++j) res[imin][j] = ++val;
+            if (++imin > imax) break;
+            for (int i=imin; i<=imax; ++i) res[i][jmax] = ++val;
+            if (--jmax < jmin) break;
+            for (int j=jmax; j>=jmin; --j) res[imax][j] = ++val; 
+            if (--imax < imin) break;
+            for (int i=imax; i>=imin; --i) res[i][jmin] = ++val;
+            if (++jmin > jmax) break;
+        }
+        return res;
+    }
+};
