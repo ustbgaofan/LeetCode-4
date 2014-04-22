f99d3f086deec23c50dc9b6dca85e23919c8e4f3
Shichao Jin <jsc0218@gmail.com>
2014-04-22 09:49:00 +0800

    format

--- Convert Sorted Array to Binary Search Tree.h
+++ Convert Sorted Array to Binary Search Tree.h
@@ -12,18 +12,19 @@ Given an array where elements are sorted in ascending order, convert it to a hei
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+ 
 class Solution {
 public:
-    TreeNode *sortedArrayToBST(vector<int> &num) {
-        return sortedArrayToBST(num, 0, num.size()-1);
-    }
-    
-    TreeNode *sortedArrayToBST(vector<int> &num, int begin, int end) {
+	TreeNode *sortedArrayToBST(vector<int> &num, int begin, int end) {
         if (begin > end) return NULL;
         int mid = (begin + end) / 2;
         TreeNode *root = new TreeNode(num[mid]);
         root->left = sortedArrayToBST(num, begin, mid-1);
         root->right = sortedArrayToBST(num, mid+1, end);
         return root;
+    }
+	
+    TreeNode *sortedArrayToBST(vector<int> &num) {
+        return sortedArrayToBST(num, 0, num.size()-1);
     }
 };
