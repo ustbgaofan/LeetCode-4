6e79910faed584b6a38ebeb46dc4d978ffa07a99
Shichao Jin <jsc0218@gmail.com>
2014-05-01 10:22:07 +0800

    add dp

--- Regular Expression Matching.h
+++ Regular Expression Matching.h
@@ -51,3 +51,47 @@ public:
         }
     }
 };
+
+// Bottom-up Dynamic Programming, time complexity O(mn), space complexity O(mn)
+class Solution {
+public:
+    bool match(char a, char b){
+        return a=='.'? true: a==b;
+    }
+    
+    bool isMatch(const char *s, const char *p) {
+        int N = strlen(s), M = strlen(p);
+        vector<vector<bool>> mem(M+1, vector<bool>(N+1, false));
+        mem[0][0] = true; 
+        for (int i=1; i<=M; ++i) {
+            mem[i][0] = p[i-1]=='*'? mem[i-2][0]: false;
+            for (int j=1; j<=N; ++j) {
+                if (p[i-1] != '*') mem[i][j] = match(p[i-1], s[j-1])? mem[i-1][j-1]: false;
+                else mem[i][j] = match(p[i-2], s[j-1])? (mem[i-2][j] || mem[i][j-1]): mem[i-2][j];
+            }
+        }
+        return mem[M][N];
+    }
+};
+
+// Space Optimized Bottom-up Dynamic Programming, time complexity O(mn), space complexity O(n)
+class Solution {
+public:
+    bool match(char a, char b){
+        return a=='.'? true: a==b;
+    }
+    
+    bool isMatch(const char *s, const char *p) {
+        int N = strlen(s), M = strlen(p);
+        vector<vector<bool>> mem(3, vector<bool>(N+1, false));
+        mem[0][0] = true; 
+        for (int i=1; i<=M; ++i) {
+            mem[i%3][0] = p[i-1]=='*'? mem[(i-2)%3][0]: false;
+            for (int j=1; j<=N; ++j) {
+                if (p[i-1] != '*') mem[i%3][j] = match(p[i-1], s[j-1])? mem[(i-1)%3][j-1]: false;
+                else mem[i%3][j] = match(p[i-2], s[j-1])? (mem[(i-2)%3][j] || mem[i%3][j-1]): mem[(i-2)%3][j];
+            }
+        }
+        return mem[M%3][N];
+    }
+};
