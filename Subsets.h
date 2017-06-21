d5f327019135be0d35ecddbfe7bf902c83c106bc
jsc0218 <jsc0218@gmail.com>
2017-06-20 21:13:10 -0400

    Update Subsets.h

--- Subsets.h
+++ Subsets.h
@@ -24,62 +24,59 @@ If S = [1,2,3], a solution is:
 // Version 1, standard DFS, transformation from Combinations version 1  
 class Solution {
 public:
-    void DFS(const vector<int> &S, vector<vector<int>> &res, int len, vector<int> &path, int begin) {
-        if (len == path.size()) {
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res(1);
+        vector<int> path;
+        for (int i=1; i<=nums.size(); ++i) DFS(nums, i, res, path, 0);
+        return res;
+    }
+    
+    void DFS(const vector<int>& nums, int len, vector<vector<int>>& res, vector<int>& path, int begin) {
+        if (path.size() == len) {
             res.push_back(path);
             return;
         }
-        int end = S.size() - (len - 1) + path.size();
-        for (int i=begin; i<end; ++i) {
-            path.push_back(S[i]);
-            DFS(S, res, len, path, i+1);
+        int end = nums.size() - (len - path.size());
+        for (int i=begin; i<=end; ++i) {
+            path.push_back(nums[i]);
+            DFS(nums, len, res, path, i+1);
             path.pop_back();
         }
-    }
-    
-    vector<vector<int> > subsets(vector<int> &S) {
-		sort(S.begin(), S.end());
-        vector<vector<int>> res(1);
-        vector<int> path;
-        for (int i=1; i<=S.size(); ++i) DFS(S, res, i, path, 0);
-        return res;
     }
 };
 
 // Version 2, every element has 2 choices
 class Solution {
 public:
-    void DFS(const vector<int> &S, vector<vector<int>> &res, vector<int> &path, int begin) {
-        if (begin == S.size()) {
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> path;
+        DFS(nums, res, path, 0);
+        return res;
+    }
+    
+    void DFS(const vector<int>& nums, vector<vector<int>>& res, vector<int>& path, int begin) {
+        if (begin == nums.size()) {
             res.push_back(path);
             return;
         }
-        path.push_back(S[begin]);
-        DFS(S, res, path, begin+1);
+        DFS(nums, res, path, begin+1);
+        path.push_back(nums[begin]);
+        DFS(nums, res, path, begin+1);
         path.pop_back();
-        DFS(S, res, path, begin+1);
-    }
-    
-    vector<vector<int> > subsets(vector<int> &S) {
-        sort(S.begin(), S.end());
-        vector<vector<int>> res;
-        vector<int> path;
-        DFS(S, res, path, 0);
-        return res;
     }
 };
 
-// Version 3, iterative version of version 2
+// Version 3, push nums[i] at the end
 class Solution {
 public:
-    vector<vector<int> > subsets(vector<int> &S) {
-		sort(S.begin(), S.end());
+    vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> res(1);
-        for (int i=0; i<S.size(); ++i) {
-            int size = res.size();
-            for (int j=0; j<size; ++j) {
+        for (int i=0; i<nums.size(); ++i) {
+            int k = res.size();
+            for (int j=0; j<k; ++j) {
                 res.push_back(res[j]);
-                res.back().push_back(S[i]);
+                res.back().push_back(nums[i]);
             }
         }
         return res;
