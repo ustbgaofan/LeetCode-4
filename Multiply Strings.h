43cdc1322186e3762b31254d4de621ed8440a1f8
jsc0218 <jsc0218@gmail.com>
2017-06-21 21:25:13 -0400

    Update Multiply Strings.h

--- Multiply Strings.h
+++ Multiply Strings.h
@@ -1,23 +1,27 @@
 // Multiply Strings
 /*
-Given two numbers represented as strings, return multiplication of the numbers as a string.
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2.
 
-Note: The numbers can be arbitrarily large and are non-negative.
+Note:
+
+The length of both num1 and num2 is < 110.
+Both num1 and num2 contains only digits 0-9.
+Both num1 and num2 does not contain any leading zero.
+You must not use any built-in BigInteger library or convert the inputs to integer directly.
 */
 
 
 class Solution {
 public:
     string multiply(string num1, string num2) {
-        int N = num1.size(), M = num2.size();
-        string res(N+M, '0');
-        for (int i=M-1; i>=0; --i) {
+        int M = num1.size(), N = num2.size();
+        string res(M+N, '0');
+        for (int i=N-1; i>=0; --i) {
             int carry = 0;
-            for (int j=N-1; j>=0; --j) {
-                int tmp = (num2[i]-'0') * (num1[j]-'0') + (res[i+1+j]-'0') + carry;
+            for (int j=M-1; j>=0; --j) {
+                int tmp = (num2[i]-'0') * (num1[j]-'0') + carry + (res[i+j+1]-'0');
                 carry = tmp / 10;
-                tmp %= 10;
-                res[i+1+j] = tmp + '0';
+                res[i+j+1] = tmp % 10 + '0';
             }
             if (carry > 0) res[i] = carry + '0';
         }
