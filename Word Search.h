ed92eedb38edb0f2c92a3319f6e3d5df23352d55
jsc0218 <jsc0218@gmail.com>
2017-06-21 22:18:30 -0400

    Update Word Search.h

--- Word Search.h
+++ Word Search.h
@@ -21,32 +21,24 @@ word = "ABCB", -> returns false.
 // time complexity O(mn * 4^(k-1)), extra space complexity O(k)
 class Solution {
 public:
-    bool DFS(vector<vector<char>> &board, int row, int col, string &word, int i) {
-        if (i+1 == word.size()) return true;
-        char tmp = board[row][col];
-        board[row][col] = '#';
-        int m = board.size(), n = board[0].size();
-        if (col > 0 && board[row][col-1]!='#' && board[row][col-1]==word[i+1] 
-            && DFS(board,row,col-1,word,i+1)) return true;
-        if (col+1<n && board[row][col+1]!='#' && board[row][col+1]==word[i+1] 
-            && DFS(board,row,col+1,word,i+1)) return true;
-        if (row > 0 && board[row-1][col]!='#' && board[row-1][col]==word[i+1] 
-            && DFS(board,row-1,col,word,i+1)) return true;
-        if (row+1<m && board[row+1][col]!='#' && board[row+1][col]==word[i+1] 
-            && DFS(board,row+1,col,word,i+1)) return true;
-        board[row][col] = tmp;
+    bool exist(vector<vector<char>>& board, string word) {
+        for (int i=0; i<board.size(); ++i) {
+            for (int j=0; j<board[0].size(); ++j) {
+                if (board[i][j]==word[0] && DFS(board, i, j, word, 1)) return true;
+            }
+        }
         return false;
     }
     
-    bool exist(vector<vector<char> > &board, string word) {
-        int m = board.size(), n = board[0].size();
-        for (int i=0; i<m; ++i) {
-            for (int j=0; j<n; ++j) {
-                if (board[i][j]==word[0] && DFS(board,i,j,word,0)) {
-                    return true;
-                }
-            }
-        }
+    bool DFS(vector<vector<char>>& board, int row, int col, string word, int i) {
+        if (i == word.size()) return true;
+        char tmp = board[row][col];
+        board[row][col] = '#';
+        if (row>0 && board[row-1][col]==word[i] && DFS(board, row-1, col, word, i+1)) return true;
+        if (col+1<board[0].size() && board[row][col+1]==word[i] && DFS(board, row, col+1, word, i+1)) return true;
+        if (row+1<board.size()>0 && board[row+1][col]==word[i] && DFS(board, row+1, col, word, i+1)) return true;
+        if (col>0 && board[row][col-1]==word[i] && DFS(board, row, col-1, word, i+1)) return true;
+        board[row][col] = tmp;
         return false;
     }
 };
