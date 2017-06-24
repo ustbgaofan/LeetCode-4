3d1e6847a14870e976baa4e163890145d053e438
jsc0218 <jsc0218@gmail.com>
2017-06-23 20:43:07 -0400

    Update Subsets II.h

--- Subsets II.h
+++ Subsets II.h
@@ -48,46 +48,45 @@ public:
 // Version 2, every element has 2 choices
 class Solution {
 public:
-    void DFS(const vector<int> &S, vector<vector<int>> &res, vector<int> &path, int begin, int count) {
-        if (begin == S.size()) {
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
+        vector<int> path;
+        DFS(nums, res, path, 0);
+        return res;
+    }
+    
+    void DFS(const vector<int>& nums, vector<vector<int>>& res, vector<int>& path, int begin) {
+        if (begin == nums.size()) {
             res.push_back(path);
             return;
         }
-        int nextCount = count;
-        if (begin+1<S.size() && S[begin+1]==S[begin]) ++nextCount;
-        else nextCount = 0;
-        if (count==0 || path.size()>=count && path[path.size()-count]==S[begin]) {
-            path.push_back(S[begin]);
-            DFS(S, res, path, begin+1, nextCount);
+        DFS(nums, res, path, begin+1);
+        int prev = begin - 1;
+        while (prev>=0 && nums[begin]==nums[prev]) --prev;
+        int sameCnt = begin - 1 - prev;
+        if (sameCnt==0 || path.size()>=sameCnt && path[path.size()-sameCnt]==nums[begin]) {
+            path.push_back(nums[begin]);
+            DFS(nums, res, path, begin+1);
             path.pop_back();
         }
-        DFS(S, res, path, begin+1, nextCount);
-    }
-    
-    vector<vector<int> > subsetsWithDup(vector<int> &S) {
-        sort(S.begin(), S.end());
-        vector<vector<int>> res;
-        vector<int> path;
-        DFS(S, res, path, 0, 0);
-        return res;
     }
 };
 
 // Version 3, iterative version of version 2
 class Solution {
 public:
-    vector<vector<int> > subsetsWithDup(vector<int> &S) {
-        sort(S.begin(), S.end());
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         vector<vector<int>> res(1);
-        for (int i=0, count=0; i<S.size(); ++i) {
-            if (i>0 && S[i]==S[i-1]) ++count;
-            else count = 0;
-            int M = res.size();
-            for (int j=0; j<M; ++j) {
+        for (int i=0, cnt=0; i<nums.size(); ++i) {
+            (i>0 && nums[i]==nums[i-1]) ?  ++cnt : cnt=0;
+            int k = res.size();
+            for (int j=0; j<k; ++j) {
                 int N = res[j].size();
-                if (count==0 || N>=count && res[j][N-count]==S[i]) {
+                if (cnt==0 || N>=cnt && nums[i]==res[j][N-cnt]) {
                     res.push_back(res[j]);
-                    res.back().push_back(S[i]);
+                    res.back().push_back(nums[i]);
                 }
             }
         }
