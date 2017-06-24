e616f3c8baef32fcac82c47db475bf9c4c70282d
jsc0218 <jsc0218@gmail.com>
2017-06-24 17:09:09 -0400

    Update Merge Sorted Array.h

--- Merge Sorted Array.h
+++ Merge Sorted Array.h
@@ -1,37 +1,35 @@
 // Merge Sorted Array
 /*
-Given two sorted integer arrays A and B, merge B into A as one sorted array.
+Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
 
 Note:
-You may assume that A has enough space to hold additional elements from B. The number of elements initialized in A and B are m and n respectively.
+You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2. The number of elements initialized in nums1 and nums2 are m and n respectively.
 */
 
 
-// Forward Version, extra space
+// Forward Version, time O(m+n), space O(m)
 class Solution {
 public:
-    void merge(int A[], int m, int B[], int n) {
-        int *C = new int[m+n];
-        int p = 0, q = 0;
-        for (int i=0; i<m+n; ++i) {
-            if (p<m && q<n) C[i] = A[p]<=B[q] ? A[p++]: B[q++];
-            else if (p == m) C[i] = B[q++];
-            else if (q == n) C[i] = A[p++];
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> tmp(nums1.begin(), nums1.begin()+m);
+        int i = 0, j = 0, k = 0;
+        while (i<m && j<n) {
+            nums1[k++] = (tmp[i] <= nums2[j]) ? tmp[i++]: nums2[j++];
         }
-        for (int i=0; i<m+n; ++i) A[i] = C[i];
-        delete[] C;
+        while (i < m) nums1[k++] = tmp[i++];
+        while (j < n) nums1[k++] = nums2[j++];
     }
 };
 
-// Backward Version, constant space
+// Backward Version, time O(m+n), space O(1)
 class Solution {
 public:
-    void merge(int A[], int m, int B[], int n) {
-        int p = m - 1, q = n - 1;
-        for (int i=m+n-1; i>=0; --i) {
-            if (p>=0 && q>=0) A[i] = A[p]>=B[q]? A[p--]:B[q--];
-            else if (p < 0) A[i] = B[q--];
-            else if (q < 0) A[i] = A[p--];
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i>=0 && j>=0) {
+            nums1[k--] = (nums1[i] > nums2[j]) ? nums1[i--] : nums2[j--];
         }
+        while (i>=0) nums1[k--] = nums1[i--];
+        while (j>=0) nums1[k--] = nums2[j--];
     }
 };
