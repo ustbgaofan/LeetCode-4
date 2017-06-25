91f6b2f6f6d237dde4bbef1695d66e924c6f1bc2
jsc0218 <jsc0218@gmail.com>
2017-06-25 15:55:56 -0400

    Update Sqrt(x).h

--- Sqrt(x).h
+++ Sqrt(x).h
@@ -7,15 +7,15 @@ Compute and return the square root of x.
 
 class Solution {
 public:
-    int sqrt(int x) {
-        int l = 0, r = (x>>1) + 1;
+    int mySqrt(int x) {
+        int l = 0, r = x; // r = x/2+1
         while (l <= r) {
-            int m = (l + r) >> 1;
-            long long t = m;  // !!!wrong: t = m*m
+            int m = l + (r-l)/2;
+            long long t = m;  // long long t = m*m (wrong!)
             t *= m;
-            if (t > x) r = m - 1;
-            else if (t < x) l = m + 1;
-            else return m;
+            if (t > x) r = m-1;
+            if (t < x) l = m+1;
+            if (t == x) return m;
         }
         return r;
     }
