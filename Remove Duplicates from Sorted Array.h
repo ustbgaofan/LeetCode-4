00137c87590356bc724c705b663f6f79db407931
jsc0218 <jsc0218@gmail.com>
2017-06-28 16:34:36 -0400

    Update Remove Duplicates from Sorted Array.h

--- Remove Duplicates from Sorted Array.h
+++ Remove Duplicates from Sorted Array.h
@@ -5,52 +5,48 @@ Given a sorted array, remove the duplicates in place such that each element appe
 Do not allocate extra space for another array, you must do this in place with constant memory.
 
 For example,
-Given input array A = [1,1,2],
+Given input array nums = [1,1,2],
 
-Your function should return length = 2, and A is now [1,2].
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
 */
 
 
 // Inexperienced Version
 class Solution {
 public:
-    int removeDuplicates(int A[], int n) {
-        if (n == 0) return n;
-        int j = 0;
-        for (int i=1; i<n; ++i) {
-            if (A[j] != A[i]) A[++j] = A[i];
-            else {
-                while (A[i]==A[j] && i<n-1) ++i;
-                if (i==n-1 && A[i]==A[j]) break;
-                else A[++j] = A[i];
-            }
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() < 2) return nums.size();
+        int j = 1;
+        for (int i=1; i<nums.size(); ) {
+            while (i<nums.size() && nums[i]==nums[i-1]) ++i;
+            if (i<nums.size()) nums[j++] = nums[i++];
         }
-        return j+1;
+        return j;
     }
 };
 
 // Experienced version
-// It hides the else logic above in the for loop 
 class Solution {
 public:
-    int removeDuplicates(int A[], int n) {
-        if (n == 0) return n;
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
         int j = 0;
-        for (int i=1; i<n; ++i)
-            if (A[i] != A[j]) A[++j] = A[i];
-        return j+1;
+        for (int i=1; i<nums.size(); ++i) {
+            if (nums[i] != nums[j]) nums[++j] = nums[i];
+        }
+        return j + 1;
     }
 };
 
 // Experienced version
-// A[i] != A[j] <==> A[i] != A[i-1]
 class Solution {
 public:
-    int removeDuplicates(int A[], int n) {
-        if (n == 0) return n;
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.empty()) return 0;
         int j = 0;
-        for (int i=1; i<n; ++i)
-            if (A[i] != A[i-1]) A[++j] = A[i];
-        return j+1;
+        for (int i=1; i<nums.size(); ++i) {
+            if (nums[i] != nums[i-1]) nums[++j] = nums[i];
+        }
+        return j + 1;
     }
 };
