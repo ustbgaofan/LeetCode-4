bcbe4edcf032a676ed1c485730f3485facd0a569
jsc0218 <jsc0218@gmail.com>
2017-07-01 19:13:34 -0400

    Update Search in Rotated Sorted Array.h

--- Search in Rotated Sorted Array.h
+++ Search in Rotated Sorted Array.h
@@ -57,3 +57,23 @@ public:
         return -1;
     }
 };
+
+// time complexity O(logn), space complexity O(1)
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int m = l + (r-l)/2;
+            if (nums[m] == target) return m;
+            if (nums[m] < nums[r]) { // right side is sorted
+                if (target>nums[m] && target<=nums[r]) l = m + 1;
+                else r = m - 1;
+            } else {  // left side is sorted
+                if (target>=nums[l] && target<nums[m]) r = m - 1;
+                else l = m + 1;
+            }
+        }
+        return -1;
+    }
+};
