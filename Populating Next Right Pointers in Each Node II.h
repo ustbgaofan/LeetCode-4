d2adf1a83d28a780458b03882acb703b44fba058
jsc0218 <jsc0218@gmail.com>
2017-07-01 16:10:12 -0400

    Update Populating Next Right Pointers in Each Node II.h

--- Populating Next Right Pointers in Each Node II.h
+++ Populating Next Right Pointers in Each Node II.h
@@ -36,24 +36,25 @@ class Solution {
 public:
     void connect(TreeLinkNode *root) {
         if (!root) return;
-        queue<TreeLinkNode *> q;
+        queue<TreeLinkNode*> q;
         q.push(root);
-        q.push(NULL);
-        while (!q.empty()) {
-            root = q.front();
+        q.push(nullptr);
+        while (q.size() > 1) {
+            TreeLinkNode* f = q.front();
             q.pop();
-            if (root) {
-                if (root->left) q.push(root->left);
-                if (root->right) q.push(root->right);
-                root->next = q.front();
-            } else if (!q.empty()) {
-                q.push(NULL);
+            if (!f) {
+                q.push(nullptr);
+                continue;
             }
+            if (f->left) q.push(f->left);
+            if (f->right) q.push(f->right);
+            f->next = q.front();
         }
+        return;
     }
 };
 
-// Recursive Version, time complexity O(n)~???, space complexity O(h)
+// (Not suggested) Recursive Version, time complexity O(n), space complexity O(h)
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
@@ -65,12 +66,12 @@ public:
             if (root->right) root->right->next = next->left? next->left: next->right;
             else if (root->left) root->left->next = next->left? next->left: next->right;
         }
-        connect(root->right);
-        connect(root->left);
+        connect(root->right);  // right first
+        connect(root->left);   // left second
     }
 };
 
-// Advanced Iterative Version 1, time complexity O(n), space complexity O(1)
+// (Not suggested) Advanced Iterative Version 1, time complexity O(n), space complexity O(1)
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
@@ -92,7 +93,7 @@ public:
     }
 };
 
-// Advanced Iterative Version 2, time complexity O(n), space complexity O(1)
+// (Not suggested) Advanced Iterative Version 2, time complexity O(n), space complexity O(1)
 class Solution {
 public:
     void connect(TreeLinkNode *root) {
@@ -108,6 +109,52 @@ public:
                 }
                 cur = cur->next;
             }
+        }
+    }
+};
+
+// Dummy head recursive version, time complexity O(n), space complexity O(h)
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        if (!root) return;
+        TreeLinkNode dummy(0), *cur = &dummy;
+        if (root->left) {
+            cur->next = root->left;
+            cur = cur->next;
+        }
+        if (root->right) {
+            cur->next = root->right;
+            cur = cur->next;
+        }
+        TreeLinkNode *next = root->next; 
+        while (next && !next->left && !next->right) next = next->next;
+        if (next) {
+            cur->next = next->left ? next->left : next->right;
+        }
+        connect(root->right);  // right first
+        connect(root->left);   // left second
+    }
+};
+
+// Dummy head iterative version, time complexity O(n), space complexity O(1)
+class Solution {
+public:
+    void connect(TreeLinkNode *root) {
+        while (root) {
+            TreeLinkNode dummy(0), *cur = &dummy;
+            while (root) {
+                if (root->left) {
+                    cur->next = root->left;
+                    cur = cur->next;
+                }
+                if (root->right) {
+                    cur->next = root->right;
+                    cur = cur->next;
+                }
+                root = root->next;
+            }
+            root = dummy.next;
         }
     }
 };
