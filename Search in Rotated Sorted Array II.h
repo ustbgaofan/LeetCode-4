806e4e6dc6a86381fe03f83b1b0818a8f3e3ffc7
jsc0218 <jsc0218@gmail.com>
2017-07-01 19:24:46 -0400

    Update Search in Rotated Sorted Array II.h

--- Search in Rotated Sorted Array II.h
+++ Search in Rotated Sorted Array II.h
@@ -4,12 +4,17 @@ Follow up for "Search in Rotated Sorted Array":
 What if duplicates are allowed?
 
 Would this affect the run-time complexity? How and why?
+Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
 
 Write a function to determine if a given target is in the array.
+
+The array may contain duplicates.
 */
 
 
-// 1st Version, time complexity O(log(n))~O(n), space complexity O(1) 
+// (not suggested) 1st Version, time complexity O(log(n))~O(n), space complexity O(1) 
 class Solution {
 public:
     bool search(int A[], int n, int target) {
@@ -41,19 +46,19 @@ public:
 // 2nd Version, time complexity O(log(n))~O(n), space complexity O(1) 
 class Solution {
 public:
-    bool search(int A[], int n, int target) {
-        int l = 0, r = n - 1;
+    bool search(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
         while (l <= r) {
-            int m = (l + r) >> 1;
-            if (target == A[m]) return true; 
-            if (A[m] > A[l]) { 
-                if (target>=A[l] && target<A[m]) r = m - 1;
-                else l = m + 1;
-            } else if (A[m] < A[l]) { 
-                if (target<=A[r] && target>A[m]) l = m + 1;
+            int m = l + (r-l)/2;
+            if (nums[m] == target) return true;
+            if (nums[m] < nums[r]) { // right side is sorted
+                if (target>nums[m] && target<=nums[r]) l = m + 1;
                 else r = m - 1;
+            } else if (nums[m] > nums[r]) {  // left side is sorted
+                if (target>=nums[l] && target<nums[m]) r = m - 1;
+                else l = m + 1;
             } else {
-                ++l;
+                --r;
             }
         }
         return false;
