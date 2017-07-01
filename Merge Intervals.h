891a05ba75f91584e5579ffd71fe600b2ee2237f
jsc0218 <jsc0218@gmail.com>
2017-06-30 20:11:20 -0400

    Update Merge Intervals.h

--- Merge Intervals.h
+++ Merge Intervals.h
@@ -17,23 +17,22 @@ return [1,6],[8,10],[15,18].
  * };
  */
 
-bool compare(const Interval &i, const Interval &j) { 
-    return i.start < j.start;
-}
-
+// time O(nlogn), space O(1)
 class Solution {
 public:
-    vector<Interval> merge(vector<Interval> &intervals) {
-        sort(intervals.begin(), intervals.end(), compare);
-        vector<Interval> res;
-        int N = intervals.size();
-        for (int i=0; i<N; ) {
-            int j = i + 1;
-            for (; j<N && intervals[i].end>=intervals[j].start; ++j) {
-                intervals[i].end = max(intervals[i].end, intervals[j].end);
+    vector<Interval> merge(vector<Interval>& intervals) {
+        if (intervals.empty()) return vector<Interval>();
+        sort(intervals.begin(), intervals.end(), [](const Interval& x, const Interval& y) {
+            return x.start < y.start;
+        });
+        vector<Interval> res(1, intervals[0]);
+        for (int i=1; i<intervals.size(); ++i) {
+            if (res.back().end >= intervals[i].end) {
+            } else if (res.back().end >= intervals[i].start) {
+                res.back().end = intervals[i].end;
+            } else {
+                res.push_back(intervals[i]);
             }
-            res.push_back(intervals[i]);
-            i = j;
         }
         return res;
     }
