1f805e100ce26548d116f6587311b7f5c2cb7688
jsc0218 <jsc0218@gmail.com>
2017-07-02 18:22:02 -0400

    Update Merge k Sorted Lists.h

--- Merge k Sorted Lists.h
+++ Merge k Sorted Lists.h
@@ -11,25 +11,56 @@ Merge k sorted linked lists and return it as one sorted list. Analyze and descri
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+// time O(kn), space O(1), assuming length of each list n/k 
 class Solution {
 public:
-    ListNode *mergeKLists(vector<ListNode *> &lists) {
-        struct MyComp {
-            bool operator() (ListNode *node1, ListNode *node2) {
-                return node1->val > node2->val;
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if (lists.empty()) return nullptr;
+        if (lists.size() == 1) return lists[0];
+        ListNode* res = mergeTwoLists(lists[0], lists[1]);
+        for (int i=2; i<lists.size(); ++i) {
+            res = mergeTwoLists(res, lists[i]);
+        }
+        return res;
+    }
+    
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0), *cur = &dummy;
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                cur->next = l1;
+                l1 = l1->next;
+            } else {
+                cur->next = l2;
+                l2 = l2->next;
             }
+            cur = cur->next;
+        }
+        if (!l1) cur->next = l2;
+        if (!l2) cur->next = l1;
+        return dummy.next;
+    }
+};
+
+// time O(nlogk), space O(k), assuming length of each list n/k 
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        struct cmp{
+            bool operator()(ListNode* x, ListNode* y) { return x->val > y->val;}
         };
-        priority_queue<ListNode *, vector<ListNode *>, MyComp> pq;
+        priority_queue<ListNode*, vector<ListNode*>, cmp> pq;  // default great heap
         for (int i=0; i<lists.size(); ++i) {
             if (lists[i]) pq.push(lists[i]);
         }
-        ListNode header(0), *cur = &header;
+        ListNode dummy(0), *cur = &dummy;
         while (!pq.empty()) {
-            ListNode *node = pq.top();
+            ListNode* t = pq.top();
             pq.pop();
-            if (node->next) pq.push(node->next);
-            cur = cur->next = node;
+            if (t->next) pq.push(t->next);
+            cur = cur->next = t;
         }
-        return header.next;
+        return dummy.next;
     }
 };
