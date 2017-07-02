8b3129e1a82335f66c2ef2c82601fcfb2072df4e
jsc0218 <jsc0218@gmail.com>
2017-07-02 17:50:05 -0400

    Update Merge Two Sorted Lists.h

--- Merge Two Sorted Lists.h
+++ Merge Two Sorted Lists.h
@@ -11,41 +11,12 @@ Merge two sorted linked lists and return it as a new list. The new list should b
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
- 
-// Version 1, creating new nodes, retain the original lists 
-class Solution {
-public:
-    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        ListNode header(0), *cur = &header;
-        while (l1 && l2) {
-            if (l1->val <= l2->val) {
-                cur->next = new ListNode(l1->val);
-                l1 = l1->next;
-            } else {
-                cur->next = new ListNode(l2->val);
-                l2 = l2->next;
-            }
-            cur = cur->next;
-        }
-        while (l1) {
-            cur->next = new ListNode(l1->val);
-            l1 = l1->next;
-            cur = cur->next;
-        }
-        while (l2) {
-            cur->next = new ListNode(l2->val);
-            l2 = l2->next;
-            cur = cur->next;
-        }
-        return header.next;
-    }
-};
 
-// Version 2, using old nodes, destroy the original lists 
+// time O(n), space O(1) 
 class Solution {
 public:
-    ListNode *mergeTwoLists(ListNode *l1, ListNode *l2) {
-        ListNode header(0), *cur = &header;
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(0), *cur = &dummy;
         while (l1 && l2) {
             if (l1->val <= l2->val) {
                 cur->next = l1;
@@ -56,8 +27,8 @@ public:
             }
             cur = cur->next;
         }
-        if (l1) cur->next = l1;
-        if (l2) cur->next = l2;
-        return header.next;
+        if (!l1) cur->next = l2;
+        if (!l2) cur->next = l1;
+        return dummy.next;
     }
 };
