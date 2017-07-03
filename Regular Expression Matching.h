29cb7b97bd5b3b1e2c7e19e4029c634ae283737f
jsc0218 <jsc0218@gmail.com>
2017-07-03 17:23:44 -0400

    Update Regular Expression Matching.h

--- Regular Expression Matching.h
+++ Regular Expression Matching.h
@@ -11,79 +11,76 @@ The function prototype should be:
 bool isMatch(const char *s, const char *p)
 
 Some examples:
-isMatch("aa","a") ¡ú false
-isMatch("aa","aa") ¡ú true
-isMatch("aaa","aa") ¡ú false
-isMatch("aa", "a*") ¡ú true
-isMatch("aa", ".*") ¡ú true
-isMatch("ab", ".*") ¡ú true
-isMatch("aab", "c*a*b") ¡ú true
+isMatch("aa","a") â†’ false
+isMatch("aa","aa") â†’ true
+isMatch("aaa","aa") â†’ false
+isMatch("aa", "a*") â†’ true
+isMatch("aa", ".*") â†’ true
+isMatch("ab", ".*") â†’ true
+isMatch("aab", "c*a*b") â†’ true
 */
 
 
-// 1st Recursive Version, time complexity O(n)~O(2^n) (aaab, a*a*a*), space complexity O(n)
+// time O(n)~O(2^n) (aaab, a*a*a*), space O(n)
+// left is string, right is pattern which includes . and *.
+// * only occurs from right[1]
 class Solution {
 public:
-    bool isMatch(const char *s, const char *p) {
-        assert(s && p && *p!='*');
-        if (*p == '\0') return *s == '\0';
-        if (*(p+1) != '*') {
-            return (*p==*s || (*p=='.' && *s!='\0')) && isMatch(s+1, p+1);
-        }
-        while (*s==*p || (*p=='.' && *s!='\0')) {
-            if (isMatch(s++, p+2)) return true;
-        }
-        return isMatch(s, p+2);
-    }
-};
-
-// 2nd Recursive Version, time complexity O(n)~O(2^n) (aaab, a*a*a*), space complexity O(n)
-class Solution {
-public:
-    bool isMatch(const char *s, const char *p) {
-        assert(s && p && *p!='*');
-        if (*p == '\0') return *s == '\0';
-        if (*p==*s || (*p=='.' && *s!='\0')) {
-            if (*(p+1) == '*') return isMatch(s, p+2) || isMatch(s+1, p);
-            else return isMatch(s+1, p+1);
-        } else { 
-            return *(p+1)=='*' && isMatch(s, p+2);
+    bool isMatch(string s, string p) {
+        if (p.empty()) return s.empty();
+        if (p.size() == 1) return s.size()==1 && (p[0]==s[0] || p[0]=='.');
+        // case 1: when the second char of p is not '*'
+        if (p[1] != '*') {
+            if (s.empty()) return false;
+            return (p[0]==s[0] || p[0]=='.') && isMatch(s.substr(1), p.substr(1));
+        } else {
+        // case 2: when the second char of p is *, complex case.
+            //case 2.1: a * can stand for 0 element
+            if (isMatch(s, p.substr(2))) return true;
+            // case 2.2: a * can stand for 1 or more preceding element, so try every sub string
+            for (int i=0; i<s.size() && (s[i]==p[0] || p[0]=='.'); ++i) {
+                if (isMatch(s.substr(i+1), p.substr(2))) return true;
+            }
+            return false;
         }
     }
 };
 
-// Bottom-up Dynamic Programming, time complexity O(mn), space complexity O(mn)
+// Bottom-up Dynamic Programming, time O(mn), space O(mn)
+// We define the state m[i][j] to be true if s[0..i) matches p[0..j) and false otherwise. Then the state equations are:
+// m[i][j] = m[i-1][j-1], if p[j-1]!='*' && (s[i-1]==p[j-1] || p[j-1]=='.');
+// m[i][j] = m[i][j-2], if p[j-1]=='*' and the pattern repeats for 0 times;
+// m[i][j] = m[i-1][j] && (s[i-1]==p[j-2] || p[j-2]=='.'), if p[j-1]=='*' and the pattern repeats for at least 1 times.
 class Solution {
 public:
-    bool isMatch(const char *s, const char *p) {
-        int N = strlen(s), M = strlen(p);
-        vector<vector<bool>> mem(M+1, vector<bool>(N+1, false));
-        mem[0][0] = true; 
-        for (int i=1; i<=M; ++i) {
-            mem[i][0] = p[i-1]=='*'? mem[i-2][0]: false;
+    bool isMatch(string s, string p) {
+        int M = s.size(), N = p.size();
+        vector<vector<bool>> m(M+1, vector<bool>(N+1, false));
+        m[0][0] = true;
+        for (int i=0; i<=M; ++i) {
             for (int j=1; j<=N; ++j) {
-                if (p[i-1] != '*') mem[i][j] = (p[i-1]=='.' || p[i-1]==s[j-1])? mem[i-1][j-1]: false;
-                else mem[i][j] = (p[i-2]=='.' || p[i-2]==s[j-1])? (mem[i-2][j] || mem[i][j-1]): mem[i-2][j];
+                if (p[j-1] != '*') m[i][j] = i>0 && (s[i-1]==p[j-1] || p[j-1]=='.') && m[i-1][j-1];
+                else m[i][j] = m[i][j-2] || (i>0 && (s[i-1]==p[j-2] || p[j-2]=='.') && m[i-1][j]);
             }
         }
-        return mem[M][N];
+        return m[M][N];
     }
 };
 
-// Space Optimized Bottom-up Dynamic Programming, time complexity O(mn), space complexity O(n)
+// Space Optimized Bottom-up Dynamic Programming, time O(mn), space O(n)
 class Solution {
 public:
-    bool isMatch(const char *s, const char *p) {
-        int N = strlen(s), M = strlen(p);
-        vector<vector<bool>> mem(3, vector<bool>(N+1, false));
-        mem[0][0] = true; 
-        for (int i=1; i<=M; ++i) {
-            mem[i%3][0] = p[i-1]=='*'? mem[(i-2)%3][0]: false;
+    bool isMatch(string s, string p) {
+        int M = s.size(), N = p.size();
+        vector<vector<bool>> m(3, vector<bool>(N+1, false));
+        m[0][0] = true;
+        for (int i=0; i<=M; ++i) {
+            if (i > 2) m[0][0] = false;
             for (int j=1; j<=N; ++j) {
-                if (p[i-1] != '*') mem[i%3][j] = (p[i-1]=='.' || p[i-1]==s[j-1])? mem[(i-1)%3][j-1]: false;
-                else mem[i%3][j] = (p[i-2]=='.' || p[i-2]==s[j-1])? (mem[(i-2)%3][j] || mem[i%3][j-1]): mem[(i-2)%3][j];
+                if (p[j-1] != '*') m[i%3][j] = i>0 && (s[i-1]==p[j-1] || p[j-1]=='.') && m[(i-1)%3][j-1];
+                else m[i%3][j] = m[i%3][j-2] || (i>0 && (s[i-1]==p[j-2] || p[j-2]=='.') && m[(i-1)%3][j]);
             }
         }
-        return mem[M%3][N];
+        return m[M%3][N];
     }
 };
