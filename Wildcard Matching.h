34b1a64e375e5af8f8aa5de4afb0dd7ea5b5443c
jsc0218 <jsc0218@gmail.com>
2017-07-04 09:51:57 -0400

    Update Wildcard Matching.h

--- Wildcard Matching.h
+++ Wildcard Matching.h
@@ -40,45 +40,42 @@ public:
     }
 };
 
-// Bottom-up Dynamic Programming, Memory Limit Exceeded, time complexity O(nm), space complexity O(nm)
+// Bottom-up Dynamic Programming, time O(nm), space O(nm)
+// We define the state m[i][j] to be true if s[0..i) matches p[0..j) and false otherwise. Then the state equations are:
+// m[i][j] = m[i-1][j-1], if p[j-1]!='*' && (s[i-1]==p[j-1] || p[j-1]=='?');
+// m[i][j] = m[i][j-1], if p[j-1]=='*' and the pattern repeats for 0 times;
+// m[i][j] = m[i-1][j], if p[j-1]=='*' and the pattern repeats for at least 1 times.
 class Solution {
 public:
-    bool isMatch(const char *s, const char *p) {
-        int N = strlen(s), M = strlen(p);
-        vector<vector<bool>> mem(M+1, vector<bool>(N+1, false));
-        mem[0][0] = true;
-        for (int i=1; i<=M; ++i) {
-            mem[i][0] = p[i-1]=='*'? mem[i-1][0]: false;
+    bool isMatch(string s, string p) {
+        int M = s.size(), N = p.size();
+        vector<vector<bool>> m(M+1, vector<bool>(N+1, false));
+        m[0][0] = true;
+        for (int i=0; i<=M; ++i) {
             for (int j=1; j<=N; ++j) {
-                if (p[i-1] != '*') mem[i][j] = (p[i-1]=='?' || p[i-1]==s[j-1])? mem[i-1][j-1]: false;
-                else mem[i][j] = mem[i-1][j] || mem[i][j-1];
+                if (p[j-1] != '*') m[i][j] = i>0 && (s[i-1]==p[j-1] || p[j-1]=='?') && m[i-1][j-1];
+                else m[i][j] = m[i][j-1] || (i>0 && m[i-1][j]);
             }
         }
-        return mem[M][N];
+        return m[M][N];
     }
 };
 
-// Space Optimized Bottom-up Dynamic Programming, time complexity O(nm), space complexity O(n)
+// Space Optimized Bottom-up Dynamic Programming, time O(nm), space O(n)
 class Solution {
 public:
-    bool isMatch(const char *s, const char *p) {
-        int N = strlen(s), M = strlen(p);
-		for (int i=0, count=0; p[i]!='\0'; ++i) {  // in order to pass
-			if (p[i]!='*' && N<++count) return false;  
-		}
-        vector<bool> mem(N+1, false);
-        mem[0] = true;
-        for (int i=1; i<=M; ++i) {
-            bool t1 = mem[0];
-            mem[0] = p[i-1]=='*'? mem[0]: false;
+    bool isMatch(string s, string p) {
+        int M = s.size(), N = p.size();
+        vector<vector<bool>> m(2, vector<bool>(N+1, false));
+        m[0][0] = true;
+        for (int i=0; i<=M; ++i) {
+            if (i > 1) m[i%2][0] = false;
             for (int j=1; j<=N; ++j) {
-                bool t2 = mem[j];
-                if (p[i-1] != '*') mem[j] = (p[i-1]=='?' || p[i-1]==s[j-1])? t1: false;
-                else mem[j] = mem[j] || mem[j-1];
-                t1 = t2;
+                if (p[j-1] != '*') m[i%2][j] = i>0 && (s[i-1]==p[j-1] || p[j-1]=='?') && m[(i-1)%2][j-1];
+                else m[i%2][j] = m[i%2][j-1] || (i>0 && m[(i-1)%2][j]);
             }
         }
-        return mem[N];
+        return m[M%2][N];
     }
 };
 
