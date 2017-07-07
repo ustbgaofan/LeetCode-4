225a715dbc20e83bc3899bdcff457556694b2354
jsc0218 <jsc0218@gmail.com>
2017-07-07 11:01:37 -0400

    Update Insert Interval.h

--- Insert Interval.h
+++ Insert Interval.h
@@ -22,85 +22,23 @@ This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
  *     Interval(int s, int e) : start(s), end(e) {}
  * };
  */
- 
-// Position-based Version with Sequential Search, time complexity O(n)
-class Solution {
-public:
-    vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
-        int N = intervals.size(), start = 0, end = 0;
-        for (int i=0; i<N && newInterval.end>intervals[end].end; ++i) {
-            if (newInterval.start > intervals[i].end) ++start;
-            if (newInterval.end > intervals[i].end) ++end;
-        }
-        vector<Interval> res;
-        for (int i=0; i<N; ++i) {
-            if (i<start || i>end) {
-                res.push_back(intervals[i]);
-            } else {
-                int intervalStart = min(newInterval.start, intervals[i].start);
-                if (end==N || newInterval.end<intervals[end].start) {
-                    res.push_back(Interval(intervalStart, newInterval.end));
-                    if (end != N) res.push_back(intervals[end]);
-                } else {
-                    res.push_back(Interval(intervalStart, intervals[end].end));
-                }
-                i = end;
-            }
-        }
-        if (start == N) res.push_back(newInterval);
-        return res;
-    }
-};
 
-// Position-based Version with Binary Search, time complexity O(log(n))~O(n)
+// time O(n), space O(1)
 class Solution {
 public:
-    int BinarySearch(const vector<Interval> &intervals, int value) {
-        int l = 0, r = intervals.size() - 1;
-        while (l <= r) {
-            int m = (l + r)/2;
-            if (value > intervals[m].end) l = m + 1;
-            else if (m>0 && value<=intervals[m-1].end) r = m - 1;
-            else return m;
-        }
-        return l;
-    }
-    
-    vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
-        int start = BinarySearch(intervals, newInterval.start);
-        int end = BinarySearch(intervals, newInterval.end);
-        int N = intervals.size();
+    vector<Interval> insert(vector<Interval>& intervals, Interval newInterval) {
         vector<Interval> res;
-        for (int i=0; i<N; ++i) {
-            if (i<start || i>end) {
-                res.push_back(intervals[i]);
-            } else {
-                int intervalStart = min(newInterval.start, intervals[i].start);
-                if (end == N || newInterval.end < intervals[end].start) {
-                    res.push_back(Interval(intervalStart, newInterval.end));
-                    if (end != N) res.push_back(intervals[end]);
-                } else {
-                    res.push_back(Interval(intervalStart, intervals[end].end));
-                }
-                i = end;
-            }
+        int i = 0, N = intervals.size(), start = newInterval.start, end = newInterval.end;
+        // add all the intervals ending before newInterval starts
+        for (; i<N && intervals[i].end<newInterval.start; ++i) res.push_back(intervals[i]);
+        // merge all overlapping intervals to one
+        for (; i<N && intervals[i].start<=newInterval.end; ++i) {
+            start = min(start, intervals[i].start);
+            end = max(end, intervals[i].end);
         }
-        if (start == N) res.push_back(newInterval);
-        return res;
-    }
-};
-
-// Value-based Version, time complexity O(n)
-class Solution {
-public:
-    vector<Interval> insert(vector<Interval> &intervals, Interval newInterval) {
-        vector<Interval> res;  
-        int i = 0, N = intervals.size(), start = newInterval.start, end = newInterval.end;  
-        for (; i<N && intervals[i].end<start; ++i) res.push_back(intervals[i]);  
-        if (i < N) start = min(start, intervals[i].start);  
-        for (; i<N && intervals[i].start<=end; ++i) end = max(end, intervals[i].end);
-        res.push_back(Interval(start, end)); 
-        res.insert(res.end(), intervals.begin()+i, intervals.end());  
+        res.push_back(Interval(start, end));
+        // add all the rest
+        for (; i<N; ++i) res.push_back(intervals[i]);
         return res;
     }
 };
