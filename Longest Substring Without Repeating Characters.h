5543ff4eac3018fb26069470f3a92da21af77347
jsc0218 <jsc0218@gmail.com>
2017-07-09 17:18:09 -0400

    Update Longest Substring Without Repeating Characters.h

--- Longest Substring Without Repeating Characters.h
+++ Longest Substring Without Repeating Characters.h
@@ -1,41 +1,29 @@
 // Longest Substring Without Repeating Characters
 /*
-Given a string, find the length of the longest substring without repeating characters. 
-For example, the longest substring without repeating letters for "abcabcbb" is "abc", 
-which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+Given a string, find the length of the longest substring without repeating characters.
+
+Examples:
+
+Given "abcabcbb", the answer is "abc", which the length is 3.
+
+Given "bbbbb", the answer is "b", with the length of 1.
+
+Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
 
-// Version 1, there are unnecessary calculations about max 
+// Sliding window + count, time O(n), space O(1)
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        bool visited[256];
-		memset(visited, 0, sizeof(visited));
-        int maxLen = 0, size = s.size();
-        for (int i=0, j=0; i<size; ++i) {
-            while (j<size && !visited[s[j]]) visited[s[j++]] = true;
-            maxLen = max(j-i, maxLen);  
-            if (j == size) break;
-            visited[s[i]] = false;
+        vector<int> m(128, 0);
+        int res = 0;
+        for (int i=0, j=0, cnt=0; j<s.size(); ) {
+            if (++m[s[j++]] == 2) ++cnt;
+            while (cnt > 0) {
+                if (--m[s[i++]] == 1) --cnt;
+            }
+            res = max(res, j-i);
         }
-        return maxLen;
-    }
-};
-
-// Version 2, no unnecessary calculations about max
-class Solution {
-public:
-    int lengthOfLongestSubstring(string s) {
-        bool visited[256];
-		memset(visited, 0, sizeof(visited));
-        int maxLen = 0, size = s.size();
-        for (int i=0, j=0; i<size; ++i) {
-            while (j<size && !visited[s[j]]) visited[s[j++]] = true;
-            maxLen = max(j-i, maxLen);
-            if (j == size) break;
-            while (s[i] != s[j]) visited[s[i++]] = false;
-            visited[s[i]] = false;
-        }
-        return maxLen;
+        return res;
     }
 };
