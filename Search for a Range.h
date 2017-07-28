66a82bd3cb9c5d17f5e7a60b1bada578f0da04d2
jsc0218 <jsc0218@gmail.com>
2017-07-27 22:06:40 -0400

    Update Search for a Range.h

--- Search for a Range.h
+++ Search for a Range.h
@@ -1,6 +1,6 @@
 // Search for a Range 
 /*
-Given a sorted array of integers, find the starting and ending position of a given target value.
+Given an array of integers sorted in ascending order, find the starting and ending position of a given target value.
 
 Your algorithm's runtime complexity must be in the order of O(log n).
 
@@ -11,29 +11,22 @@ Given [5, 7, 7, 8, 8, 10] and target value 8,
 return [3, 4].
 */
 
-// Naive Version, time complexity: worst case O(n) 
+// Naive Binary Search, time O(n), space O(1) 
 class Solution {
 public:
-    vector<int> searchRange(int A[], int n, int target) {
-        vector<int> res(2, -1);
-        int l = 0, r = n - 1;
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
         while (l <= r) {
-            int m = (l + r)/2;
-            if (target < A[m]) {
-                r = m - 1; 
-            } else if (target > A[m]) {
-                l = m + 1;
-            } else {
-                int i = m - 1;
-                for (; i>=0 && A[i]==target; --i);
-                res[0] = i + 1;
-                i = m + 1;
-                for (; i<n && A[i]==target; ++i);
-                res[1] = i - 1;
-                return res;
+            int m = l + (r-l)/2;
+            if (target > nums[m]) l = m + 1;
+            if (target < nums[m]) r = m - 1;
+            if (target == nums[m]) {
+                for (l=m-1; l>=0 && nums[l]==target; --l);
+                for (r=m+1; r<=nums.size()-1 && nums[r]==target; ++r);
+                return vector<int>({++l, --r});
             }
         }
-        return res;
+        return vector<int>({-1, -1});
     }
 };
 
