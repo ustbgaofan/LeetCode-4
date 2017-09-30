579d38e5c8ef4d952c34e0493cecfdaba1504bda
jsc0218 <jsc0218@gmail.com>
2017-09-30 11:23:48 -0400

    Update Letter Combinations of a Phone Number.h

--- Letter Combinations of a Phone Number.h
+++ Letter Combinations of a Phone Number.h
@@ -16,43 +16,37 @@ Although the above answer is in lexicographical order, your answer could be in a
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        vector<string> res;
-        if (digits.size() == 0) {
-            res.push_back(""); 
-            return res;
+        if (digits.empty()) return vector<string>();
+        vector<string> h = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> res(1, "");
+        for (int i=0; i<digits.size(); ++i) {
+            string& str = h[digits[i]-'0'];
+            vector<string> res2;
+            for (int j=0; j<res.size(); ++j) {
+                for (int k=0; k<str.size(); ++k) {
+                    res2.push_back(res[j]+str[k]);
+                }
+            }
+            res = res2;
         }
-	    string mapping[8] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-	    string str = mapping[digits[0]-'2'];
-	    for (int i=0; i<str.size(); ++i) {
-		    string t = "";
-		    res.push_back(t + str[i]);
-	    }
-	    for (int i=1; i<digits.size(); ++i) {
-		    str = mapping[digits[i]-'2'];
-		    int size1 = res.size(), size2 = str.size();
-		    vector<string> temp(res); 
-		    res.resize(size1*size2);
-		    for (int j=0; j<size1; ++j) {
-			    for (int k=0; k<size2; ++k) {
-				    res[j*size2+k] = temp[j] + str[k];
-			    }
-		    }
-	    }
-	    return res;
+        return res;
     }
 };
 
-// Advanced Version, less code, in place method.
+// Advanced Version, in place method.
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        string mapping[8] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        if (digits.empty()) return vector<string>();
+        vector<string> h = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
         vector<string> res(1, "");
         for (int i=0; i<digits.size(); ++i) {
-            string str = mapping[digits[i]-'2'];
-            for (int j=res.size()-1; j>=0; --j) {
-                for (int k=1; k<str.size(); ++k) 
-                    res.push_back(res[j] + str[k]);
+            string& str = h[digits[i]-'0'];
+            int N = res.size();
+            for (int j=0; j<N; ++j) {
+                for (int k=1; k<str.size(); ++k) {
+                    res.push_back(res[j]+str[k]);
+                }
                 res[j] += str[0];
             }
         }
@@ -63,24 +57,25 @@ public:
 // DFS Version
 class Solution {
 public:
-    void DFS(vector<string>& res, string& path, const string& digits, string mapping[]) {
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return vector<string>();
+        vector<string> h = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        vector<string> res;
+        string path;
+        DFS(res, path, digits, h);
+        return res;
+    }
+    
+    void DFS(vector<string>& res, string& path, const string& digits, const vector<string>& h) {
         if (path.size() == digits.size()) {
             res.push_back(path);
             return;
         }
-        string str = mapping[digits[path.size()]-'2'];
+        const string& str = h[digits[path.size()]-'0'];
         for (int i=0; i<str.size(); ++i) {
             path.push_back(str[i]);
-            DFS(res, path, digits, mapping);
+            DFS(res, path, digits, h);
             path.pop_back();
         }
-    }
-    
-    vector<string> letterCombinations(string digits) {
-        string mapping[8] = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        vector<string> res;
-        string path;
-        DFS(res, path, digits, mapping);
-        return res; 
     }
 };
