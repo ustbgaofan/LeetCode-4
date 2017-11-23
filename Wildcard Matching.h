dcaa9db21771e87f219d4051cdf49463554c54ae
jsc0218 <jsc0218@gmail.com>
2017-11-22 21:26:41 -0500

    Update Wildcard Matching.h

--- Wildcard Matching.h
+++ Wildcard Matching.h
@@ -26,13 +26,13 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
         if (p.empty()) return s.empty();
-        if (s.empty()) {
-            for (int i=0; i<p.size(); ++i) {
-                if (p[i] != '*') return false;
-            }
-            return true;
+        if (p.size() == 1) {
+            return (s.size()==1 && (p[0]==s[0] || p[0]=='?')) || p[0]=='*';
         }
-        if (p[0] != '*') return (p[0]==s[0] || p[0]=='?') && isMatch(s.substr(1), p.substr(1));
+        if (p[0] != '*') {
+            if (s.empty()) return false;
+            return (p[0]==s[0] || p[0]=='?') && isMatch(s.substr(1), p.substr(1));
+        }
         for (int i=0; i<=s.size(); ++i) {
             if (isMatch(s.substr(i), p.substr(1))) return true;
         }
@@ -40,7 +40,7 @@ public:
     }
 };
 
-// Bottom-up Dynamic Programming, time O(nm), space O(nm)
+// Bottom-up Dynamic Programming, time O(MN), space O(MN)
 // We define the state m[i][j] to be true if s[0..i) matches p[0..j) and false otherwise. Then the state equations are:
 // m[i][j] = m[i-1][j-1], if p[j-1]!='*' && (s[i-1]==p[j-1] || p[j-1]=='?');
 // m[i][j] = m[i][j-1], if p[j-1]=='*' and the pattern repeats for 0 times;
@@ -50,10 +50,10 @@ public:
     bool isMatch(string s, string p) {
         int M = s.size(), N = p.size();
         vector<vector<bool>> m(M+1, vector<bool>(N+1, false));
-        m[0][0] = true;
+        m[0][0] = true; 
         for (int i=0; i<=M; ++i) {
             for (int j=1; j<=N; ++j) {
-                if (p[j-1] != '*') m[i][j] = i>0 && (s[i-1]==p[j-1] || p[j-1]=='?') && m[i-1][j-1];
+                if (p[j-1] != '*') m[i][j] = i>0 && m[i-1][j-1] && (s[i-1]==p[j-1] || p[j-1]=='?');
                 else m[i][j] = m[i][j-1] || (i>0 && m[i-1][j]);
             }
         }
@@ -61,17 +61,17 @@ public:
     }
 };
 
-// Space Optimized Bottom-up Dynamic Programming, time O(nm), space O(n)
+// Space Optimized Bottom-up Dynamic Programming, time O(MN), space O(N)
 class Solution {
 public:
     bool isMatch(string s, string p) {
         int M = s.size(), N = p.size();
         vector<vector<bool>> m(2, vector<bool>(N+1, false));
-        m[0][0] = true;
+        m[0][0] = true; 
         for (int i=0; i<=M; ++i) {
-            if (i > 1) m[i%2][0] = false;
+            if (i > 1) m[0][0] = false;
             for (int j=1; j<=N; ++j) {
-                if (p[j-1] != '*') m[i%2][j] = i>0 && (s[i-1]==p[j-1] || p[j-1]=='?') && m[(i-1)%2][j-1];
+                if (p[j-1] != '*') m[i%2][j] = i>0 && m[(i-1)%2][j-1] && (s[i-1]==p[j-1] || p[j-1]=='?');
                 else m[i%2][j] = m[i%2][j-1] || (i>0 && m[(i-1)%2][j]);
             }
         }
