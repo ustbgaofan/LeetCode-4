8fe1a741d7cc89573177b22ac042e4bd93e9d184
jsc0218 <jsc0218@gmail.com>
2018-01-03 15:12:36 -0500

    Update Trapping Rain Water.h

--- Trapping Rain Water.h
+++ Trapping Rain Water.h
@@ -10,38 +10,36 @@ The above elevation map is represented by array [0,1,0,2,1,0,1,3,2,1,2,1].
 In this case, 6 units of rain water (blue section) are being trapped. Thanks Marcos for contributing this image!
 */
 
-
-// Two-array Version, time complexity O(n), space complexity O(n)
+// Brute Force, time O(n^2), space O(1)
 class Solution {
 public:
-    int trap(int A[], int n) {
-        if (n < 3) return 0;
-        vector<int> rmax(n, 0);
-        for (int i=n-2; i>=1; --i) rmax[i] = max(rmax[i+1], A[i+1]);
-        vector<int> lmax(n-1, 0);
-        for (int i=1; i<=n-2; ++i) lmax[i] = max(lmax[i-1], A[i-1]);
-        int res = 0;
-        for (int i=1; i<n-1; ++i) res += max(0, min(lmax[i], rmax[i]) - A[i]);
-        return res;
-    }
-};
-
-// One-array Version, time complexity O(n), space complexity O(n) with a smaller constant
-class Solution {
-public:
-    int trap(int A[], int n) {
-        vector<int> rmax(n, 0);
-        for (int i=n-2; i>=1; --i) rmax[i] = max(rmax[i+1], A[i+1]);
-        int res = 0;
-        for (int i=1, lmax=0; i<n-1; ++i) {
-            lmax = max(lmax, A[i-1]);
-            res += max(0, min(rmax[i], lmax) - A[i]);
+    int trap(vector<int>& height) {
+        int N = height.size(), res = 0; 
+        for (int i=0; i<N; ++i) {
+            int left = 0, right = 0;
+            for (int j=i; j>=0; --j) left = max(left, height[j]);
+            for (int j=i; j<N; ++j) right = max(right, height[j]);
+            res += min(left, right) - height[i];
         }
         return res;
     }
 };
 
-// No-array Version, time complexity O(n), space complexity O(1)
+// Dynamic Programming, time O(n), space O(n)
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        if (height.empty()) return 0;
+        int N = height.size(), res = 0;
+        vector<int> lmax(N, height[0]), rmax(N, height.back());
+        for (int i=1; i<N; ++i) lmax[i] = max(height[i], lmax[i-1]); 
+        for (int i=N-2; i>=0; --i) rmax[i] = max(height[i], rmax[i+1]);
+        for (int i=0; i<N; ++i) res += min(lmax[i], rmax[i]) - height[i];
+        return res;
+    }
+};
+
+// No-array Version, time O(n), space O(1)
 class Solution {
 public:
     int trap(int A[], int n) {
