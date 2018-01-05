eae8c0e8c1cbfec1d7c5f7dfbe2230e1041d4d56
jsc0218 <jsc0218@gmail.com>
2018-01-05 11:31:36 -0500

    Update Permutations.h

--- Permutations.h
+++ Permutations.h
@@ -1,40 +1,66 @@
 // Permutations 
 /*
-Given a collection of numbers, return all possible permutations.
+Given a collection of distinct numbers, return all possible permutations.
 
 For example,
 [1,2,3] have the following permutations:
-[1,2,3], [1,3,2], [2,1,3], [2,3,1], [3,1,2], and [3,2,1].
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
 */
 
-
-// time complexity O(n!), space complexity O(n)
+// DFS, time O(n!n), space O(n)
 class Solution {
 public:
-    void DFS(vector<vector<int>> &res, vector<int> &path, vector<pair<int, bool>> &vec) {
-        if (path.size() == vec.size()) {
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> res;
+        vector<int> path;
+        DFS(nums, res, path);
+        return res;
+    }
+    
+    void DFS(const vector<int>& nums, vector<vector<int>>& res, vector<int>& path) {
+        if (path.size() == nums.size()) {
             res.push_back(path);
             return;
         }
-        for (int i=0; i<vec.size(); ++i) {
-            if (!vec[i].second) {
-                vec[i].second = true;
-                path.push_back(vec[i].first);
-                DFS(res, path, vec);
-                vec[i].second = false;
-                path.pop_back();
-            }
+        for (int i=0; i<nums.size(); ++i) {
+            if (find(path.begin(), path.end(), nums[i]) != path.end()) continue;
+            path.push_back(nums[i]);
+            DFS(nums, res, path);
+            path.pop_back();
         }
     }
-    
-    vector<vector<int> > permute(vector<int> &num) {
+};
+
+// DFS, time O(n!), space O(n)
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
         vector<int> path;
-        vector<pair<int, bool>> vec;
-        for (int i=0; i<num.size(); ++i) {
-			vec.push_back(make_pair(num[i], false));
-		}
-        DFS(res, path, vec);
+        vector<bool> visited(nums.size(), false);
+        DFS(nums, res, path, visited);
         return res;
+    }
+    
+    void DFS(const vector<int>& nums, vector<vector<int>>& res, vector<int>& path, vector<bool>& visited) {
+        if (path.size() == nums.size()) {
+            res.push_back(path);
+            return;
+        }
+        for (int i=0; i<nums.size(); ++i) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            path.push_back(nums[i]);
+            DFS(nums, res, path, visited);
+            visited[i] = false;
+            path.pop_back();
+        }
     }
 };
