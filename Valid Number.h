3d2591af7a8fd8093bd1e17208aa1fe03807d211
jsc0218 <jsc0218@gmail.com>
2018-01-17 20:25:24 -0500

    Update Valid Number.h

--- Valid Number.h
+++ Valid Number.h
@@ -9,69 +9,35 @@ Some examples:
 "1 a" => false
 "2e10" => true
 Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up front before implementing one.
+
+Update (2015-02-10):
+The signature of the C++ function had been updated. If you still see your function signature accepts a const char * argument, please click the reload button  to reset your code definition.
 */
 
-// Really Weird!
-
+// time O(n), space O(1)
 class Solution {
 public:
-    bool isNumber(const char *s) {
-        if (s == NULL) return false;
-        while (*s == ' ') ++s;
-        if (*s=='+' || *s=='-') ++s;
-        if (*s == '\0') return false;
-        const char *begin = s, *e = strlen(s) + s - 1;
-        while (*e == ' ') --e;
-        int dotCount = 0, eCount = 0, plusCount = 0, minusCount = 0;
-        while (s <= e) {
-            if (*s=='-' && ++minusCount>0 || *s=='+' && ++plusCount>0) return false;  
-            if ((*s<'0' && *s!='.' && *s!='-' && *s!='+') || (*s>'9' && *s!='e' && *s!='E')) return false;
-            if (*s=='e' || *s=='E') {
-                if (++eCount > 1) return false;
-                if (s==begin || (!isdigit(*(s-1)) && *(s-1)!='.') || (!isdigit(*(s+1)) && *(s+1)!='-' && *(s+1)!='+')) return false;
-                if (*(s+1)=='+' || *(s+1)=='-') ++s;
-                if (!isdigit(*(s+1))) return false;
-            }
-            if (*s == '.') {
-                if (eCount || ++dotCount>1) return false;
-                if (!isdigit(*(s+1)) && (s==begin || s>begin && !isdigit(*(s-1)))) return false;
-            }
-            ++s;
+    bool isNumber(string s) {
+        int i = 0, N = s.size();
+    
+        for (; i<N && s[i]==' '; ++i);  // skip the whilespaces
+    
+        if (i<N && (s[i]=='+' || s[i]=='-')) ++i;  // skip the sign if exist 
+        
+        int num = 0, pt = 0;
+        for (; i<N && (isdigit(s[i]) || s[i]=='.'); ++i) s[i]=='.'? ++pt: ++num;
+        if (pt>1 || num<1) return false;  // no more than one point, at least one digit
+        
+        if (i<N && s[i]=='e') {
+            ++i;
+            if (i<N && s[i]=='+' || s[i]=='-') ++i;
+            for (num=0; i<N && isdigit(s[i]); ++i, ++num) ;
+            if (num < 1) return false;
         }
-        return true;
-    }
-};
-
-class Solution {
-public:
-    bool isNumber(const char *s) {
-		if (s == NULL) return false;
-        while(isspace(*s)) s++;
-        if (*s == '+' || *s == '-') s++;    
-        bool eAppear = false, dotAppear = false, firstPart = false, secondPart = false, spaceAppear = false;
-        while(*s != '\0') {
-			if (*s == '.') {
-				if (dotAppear || eAppear || spaceAppear) return false;
-                else dotAppear = true;
-            } else if (*s == 'e' || *s == 'E') {
-                if (eAppear || !firstPart || spaceAppear) return false;
-                else eAppear = true;
-            } else if (isdigit(*s)) {
-                if (spaceAppear) return false;
-                if (!eAppear) firstPart = true;
-                else secondPart = true;
-            } else if (*s == '+' || *s == '-') {
-                if (spaceAppear) return false;
-                if (!eAppear || !(*(s-1) == 'e' || *(s-1) == 'E')) return false;
-            } else if (isspace(*s)) {
-                spaceAppear = true;
-			} else {
-                return false;
-            }
-            s++;            
-        }
-        if (!firstPart) return false;
-        else if (eAppear && !secondPart) return false;
-        else return true;
+        
+        // skip the trailing whitespaces
+        for (; i<N && s[i]==' '; ++i) ;
+        
+        return i == s.size();  // must reach the ending 0 of the string
     }
 };
