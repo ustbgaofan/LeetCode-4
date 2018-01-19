350d5d91ccd3ba61bbed9377ca088814773075f9
jsc0218 <jsc0218@gmail.com>
2018-01-19 15:16:12 -0500

    Update Binary Tree Zigzag Level Order Traversal.h

--- Binary Tree Zigzag Level Order Traversal.h
+++ Binary Tree Zigzag Level Order Traversal.h
@@ -3,7 +3,7 @@
 Given a binary tree, return the zigzag level order traversal of its nodes' values. (ie, from left to right, then right to left for the next level and alternate between).
 
 For example:
-Given binary tree {3,9,20,#,#,15,7},
+Given binary tree [3,9,20,null,null,15,7],
     3
    / \
   9  20
@@ -15,25 +15,10 @@ return its zigzag level order traversal as:
   [20,9],
   [15,7]
 ]
-confused what "{1,#,2,3}" means? > read more on how binary tree is serialized on OJ.
-
-
-OJ's Binary Tree Serialization:
-The serialization of a binary tree follows a level order traversal, where '#' signifies a path terminator where no node exists below.
-
-Here's an example:
-   1
-  / \
- 2   3
-    /
-   4
-    \
-     5
-The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
 */
 
 /**
- * Definition for binary tree
+ * Definition for a binary tree node.
  * struct TreeNode {
  *     int val;
  *     TreeNode *left;
@@ -42,12 +27,12 @@ The above binary tree is serialized as "{1,2,3,#,#,4,#,#,5}".
  * };
  */
  
-// Two-stacks Version, time complexity O(n), space complexity O(n)
+// Two Stacks, time O(n), space O(n)
 class Solution {
 public:
-    vector<vector<int> > zigzagLevelOrder(TreeNode *root) {
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> res;
-        stack<TreeNode *> stk[2];
+        stack<TreeNode*> stk[2];
         int cur = 0;
         if (root) stk[cur].push(root);
         while (!stk[cur].empty()) {
@@ -71,15 +56,15 @@ public:
     }
 };
 
-// Queue + Reverse Version, time complexity O(n), space complexity O(n)
+// Queue + Reverse, time O(n), space O(n)
 class Solution {
 public:
-    vector<vector<int> > zigzagLevelOrder(TreeNode *root) {
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> res;
         int cur = 0;
-        queue<TreeNode *> q;
+        queue<TreeNode*> q;
         if (root) q.push(root);
-        q.push(NULL);
+        q.push(nullptr);
         while (q.front()) {
             vector<int> line;
             while (q.front()) {
@@ -90,7 +75,7 @@ public:
                 line.push_back(node->val);
             }
             q.pop();
-            q.push(NULL);
+            q.push(nullptr);
             if (cur == 1) reverse(line.begin(), line.end());
             cur = (++cur) % 2;
             res.push_back(line);
