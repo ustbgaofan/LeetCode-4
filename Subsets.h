67bf45ca8ca5490a59f8cf6a26c19e965f155ada
jsc0218 <jsc0218@gmail.com>
2018-01-22 15:26:58 -0500

    Update Subsets.h

--- Subsets.h
+++ Subsets.h
@@ -20,32 +20,7 @@ If S = [1,2,3], a solution is:
 ]
 */
 
-
-// Version 1, standard DFS, transformation from Combinations version 1  
-class Solution {
-public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res(1);
-        vector<int> path;
-        for (int i=1; i<=nums.size(); ++i) DFS(nums, i, res, path, 0);
-        return res;
-    }
-    
-    void DFS(const vector<int>& nums, int len, vector<vector<int>>& res, vector<int>& path, int begin) {
-        if (path.size() == len) {
-            res.push_back(path);
-            return;
-        }
-        int end = nums.size() - (len - path.size());
-        for (int i=begin; i<=end; ++i) {
-            path.push_back(nums[i]);
-            DFS(nums, len, res, path, i+1);
-            path.pop_back();
-        }
-    }
-};
-
-// Version 2, every element has 2 choices
+// Recursive, every element has 2 choices
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
@@ -67,7 +42,7 @@ public:
     }
 };
 
-// Version 3, push nums[i] at the end
+// Iterative, push nums[i] at the end
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
