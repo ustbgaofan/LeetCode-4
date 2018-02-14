bc60cfc0fd1cce3d6c59a7ad5658e4bc28efd4d5
jsc0218 <jsc0218@gmail.com>
2018-02-14 10:18:14 -0500

    Update Remove Duplicates from Sorted Array II.h

--- Remove Duplicates from Sorted Array II.h
+++ Remove Duplicates from Sorted Array II.h
@@ -9,14 +9,15 @@ Given sorted array nums = [1,1,1,2,2,3],
 Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3. It doesn't matter what you leave beyond the new length.
 */
 
+// time O(n), space O(1)
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         if (nums.size() <= 2) return nums.size();
-        int j = 2;
+        int j = 1;
         for (int i=2; i<nums.size(); ++i) {
-            if (nums[i] != nums[j-2]) nums[j++] = nums[i];
+            if (nums[i] != nums[j-1]) nums[++j] = nums[i];
         }
-        return j;
+        return j + 1;
     }
 };
