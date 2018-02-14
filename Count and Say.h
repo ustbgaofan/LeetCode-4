4a664f88e3ec1842b9d69911d51ab3cb625ac372
jsc0218 <jsc0218@gmail.com>
2018-02-14 10:59:48 -0500

    Update Count and Say.h

--- Count and Say.h
+++ Count and Say.h
@@ -1,17 +1,30 @@
 // Count and Say
 /*
-The count-and-say sequence is the sequence of integers beginning as follows:
-1, 11, 21, 1211, 111221, ...
+The count-and-say sequence is the sequence of integers with the first five terms as following:
 
+1.     1
+2.     11
+3.     21
+4.     1211
+5.     111221
 1 is read off as "one 1" or 11.
 11 is read off as "two 1s" or 21.
 21 is read off as "one 2, then one 1" or 1211.
-Given an integer n, generate the nth sequence.
+Given an integer n, generate the nth term of the count-and-say sequence.
 
-Note: The sequence of integers will be represented as a string.
+Note: Each term of the sequence of integers will be represented as a string.
+
+Example 1:
+
+Input: 1
+Output: "1"
+Example 2:
+
+Input: 4
+Output: "1211"
 */
 
-
+// time O(nk), space O(1) 
 class Solution {
 public:
     string countAndSay(int n) {
