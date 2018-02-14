236d06f3d1a902998e57bc246d3dd4b3d4b7e46b
jsc0218 <jsc0218@gmail.com>
2018-02-14 10:17:17 -0500

    Update Remove Duplicates from Sorted Array.h

--- Remove Duplicates from Sorted Array.h
+++ Remove Duplicates from Sorted Array.h
@@ -10,35 +10,7 @@ Given input array nums = [1,1,2],
 Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively. It doesn't matter what you leave beyond the new length.
 */
 
-
-// Inexperienced Version
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        if (nums.size() < 2) return nums.size();
-        int j = 1;
-        for (int i=1; i<nums.size(); ) {
-            while (i<nums.size() && nums[i]==nums[i-1]) ++i;
-            if (i<nums.size()) nums[j++] = nums[i++];
-        }
-        return j;
-    }
-};
-
-// Experienced version
-class Solution {
-public:
-    int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0;
-        int j = 0;
-        for (int i=1; i<nums.size(); ++i) {
-            if (nums[i] != nums[j]) nums[++j] = nums[i];
-        }
-        return j + 1;
-    }
-};
-
-// Experienced version
+// Less general, time O(n), space O(1)
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
@@ -51,15 +23,16 @@ public:
     }
 };
 
-// Experienced version
+// More general, time O(n), space O(1)
+// 80. Remove Duplicates from Sorted Array II
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
         if (nums.empty()) return 0;
-        int j = 1;
+        int j = 0;
         for (int i=1; i<nums.size(); ++i) {
-            if (nums[i] != nums[j-1]) nums[j++] = nums[i];
+            if (nums[i] != nums[j]) nums[++j] = nums[i];
         }
-        return j;
+        return j + 1;
     }
 };
