d87dc9288009a274db439ad66bdb9fad9f1ec042
jsc0218 <jsc0218@gmail.com>
2018-02-17 10:07:54 -0500

    Update Subsets II.h

--- Subsets II.h
+++ Subsets II.h
@@ -1,12 +1,11 @@
 // Subsets II 
 /*
-Given a collection of integers that might contain duplicates, S, return all possible subsets.
+Given a collection of integers that might contain duplicates, nums, return all possible subsets (the power set).
 
-Note:
-Elements in a subset must be in non-descending order.
-The solution set must not contain duplicate subsets.
+Note: The solution set must not contain duplicate subsets.
+
 For example,
-If S = [1,2,2], a solution is:
+If nums = [1,2,2], a solution is:
 
 [
   [2],
@@ -19,33 +18,8 @@ If S = [1,2,2], a solution is:
 */
 
 
-// Version 1, standard DFS
-class Solution {
-public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        vector<vector<int>> res(1);
-        vector<int> path;
-        for (int i=1; i<=nums.size(); ++i) DFS(nums, i, res, path, 0);
-        return res;
-    }
-    
-    void DFS(const vector<int>& nums, int len, vector<vector<int>>& res, vector<int>& path, int begin) {
-        if (path.size() == len) {
-            res.push_back(path);
-            return;
-        }
-        int end = nums.size() - (len - path.size());
-        for (int i=begin; i<=end; ++i) {
-            if (i>begin && nums[i]==nums[i-1]) continue;
-            path.push_back(nums[i]);
-            DFS(nums, len, res, path, i+1);
-            path.pop_back();
-        }
-    }
-};
-
-// Version 2, every element has 2 choices
+// (not suggested) Recursive, time O(2^n), space O(n)
+// every element has 2 choices
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
@@ -73,7 +47,8 @@ public:
     }
 };
 
-// Version 3, iterative version of version 2
+// (not suggested) Iterative, time O(2^n), space O(1)
+// push nums[i] at the end
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
@@ -94,47 +69,28 @@ public:
     }
 };
 
-// Version 2, every element has 2 choices
+// More general, time O(2^n), space O(n)
+// @ --> 1 --> 1,2 --> 1,2,3
+//         --> 1,3
+//   --> 2 --> 2,3
+//   --> 3
 class Solution {
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
         vector<int> path;
-        DFS(nums, path, 0);
-        return res;
-    }
-    
-    void DFS(const vector<int>& nums, vector<int>& path, int begin) {
-        if (begin == nums.size()) {
-            if (h.insert(path).second) res.push_back(path);
-            return;
-        }
-        DFS(nums, path, begin+1);
-        path.push_back(nums[begin]);
-        DFS(nums, path, begin+1);
-        path.pop_back();
-    }
-    
-private:
-    vector<vector<int>> res;
-    set<vector<int>> h;
-};
-
-// Version 3, iterative version of version 2
-class Solution {
-public:
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        set<vector<int>> h;
-        vector<vector<int>> res(1);
-        for (int i=0; i<nums.size(); ++i) {
-            int k = res.size();
-            for (int j=0; j<k; ++j) {
-                res.push_back(res[j]);
-                res.back().push_back(nums[i]);
-                if (!h.insert(res.back()).second) res.pop_back();
-            }
-        }
+        DFS(nums, res, path, 0);
         return res;
+    }
+    
+    void DFS(const vector<int>& nums, vector<vector<int>>& res, vector<int>& path, int i) {
+        res.push_back(path);
+        for (int j=i; j<nums.size(); ++j) {
+            if (j>i && nums[j]==nums[j-1]) continue;
+            path.push_back(nums[j]);
+            DFS(nums, res, path, j+1);
+            path.pop_back();
+        }
     }
 };
