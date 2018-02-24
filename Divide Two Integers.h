1066f898ce38cf18cb868a5f8707d65c2443897a
jsc0218 <jsc0218@gmail.com>
2018-02-23 22:23:23 -0500

    Update Divide Two Integers.h

--- Divide Two Integers.h
+++ Divide Two Integers.h
@@ -5,13 +5,13 @@ Divide two integers without using multiplication, division and mod operator.
 If it is overflow, return MAX_INT.
 */
 
-// Naive Version, Time Limit Exceeded, time O(n), where n=dividend/divisor, space O(1)
+// Brute force, time O(n), where n=dividend/divisor, space O(1)
 class Solution {
 public:
     int divide(int dividend, int divisor) {
         if (divisor==0 || (dividend==INT_MIN && divisor==-1)) return INT_MAX;
         bool sign = (dividend>0 && divisor<0) || (dividend<0 && divisor>0);
-        long long dvd = labs(dividend), dvs = labs(divisor), res = 0;
+        long long dvd = labs(dividend), dvs = labs(divisor), res = 0;  // (INT_MIN, 1)
         while (dvd >= dvs) {
             dvd -= dvs;
             ++res;
@@ -21,13 +21,13 @@ public:
 };
 
 // Bit-manipulation, time O(logn), where n=dividend/divisor, space O(1)
-// 2^k * dvs = dvd; 
+// 2^k * dvs = dvd;
 class Solution {
 public:
     int divide(int dividend, int divisor) {
         if (divisor==0 || (dividend==INT_MIN && divisor==-1)) return INT_MAX;
         bool sign = (dividend>0 && divisor<0) || (dividend<0 && divisor>0);
-        long long dvd = labs(dividend), dvs = labs(divisor), res = 0;
+        long long dvd = labs(dividend), dvs = labs(divisor), res = 0;  // (INT_MIN, 1)
         while (dvd >= dvs) {
             long long temp = dvs, multiple = 1;
             while (dvd >= (temp<<1)){
