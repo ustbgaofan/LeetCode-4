37b6b8687c337c2f20d6f1e7917988c417268364
jsc0218 <jsc0218@gmail.com>
2018-02-25 19:55:33 -0500

    Update Word Break II.h

--- Word Break II.h
+++ Word Break II.h
@@ -14,6 +14,29 @@ UPDATE (2017/1/4):
 The wordDict parameter had been changed to a list of strings (instead of a set of strings). Please reload the code definition to get the latest changes.
 */
 
+// Recursive
+class Solution {
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> dict(wordDict.begin(), wordDict.end());
+        return DFS(s, dict, 0);
+    }
+    
+    vector<string> DFS(const string& s, const unordered_set<string>& dict, int i) {
+        vector<string> res;
+        for (int j=i; j<s.size(); ++j) {
+            string path = s.substr(i, j-i+1);
+            if (dict.find(path) == dict.end()) continue;
+            if (j+1 == s.size()) {
+                res.push_back(path);
+            } else {
+                vector<string> t = DFS(s, dict, j+1);
+                for (string str : t) res.push_back(path+" "+str);
+            }
+        }
+        return res;
+    }
+};
 
 // Top-down Dynamic Programming, time O(2^n), space O(2^n)
 class Solution {
@@ -34,8 +57,8 @@ public:
             if (j+1 == s.size()) {
                 m[i].push_back(path);
             } else {
-                vector<string> res = DFS(s, dict, j+1, m, v);
-                for (int k=0; k<res.size(); ++k) m[i].push_back(path+" "+res[k]);
+                vector<string> t = DFS(s, dict, j+1, m, v);
+                for (string str : t) m[i].push_back(path+" "+str);
             }
         }
         return m[i];
@@ -43,6 +66,9 @@ public:
 };
 
 // Bottom-up Dynamic Programming, time O(2^n), space O(2^n)
+// the brute force search is to try all the options whether to insert a space at position i.
+// So the total options to search is 2^n, so the time complexity should be O(2^n) n is the length of the sentence to break
+// same thing, for the space complexity, it's same all those 2^n can be a valid break, so the space complexity is also O(2^n)
 class Solution {
 public:
     vector<string> wordBreak(string s, vector<string>& wordDict) {
@@ -57,9 +83,7 @@ public:
                 if (j+1 == N) {
                     mem[i].push_back(path);
                 } else {
-                    for (int k=0; k<mem[j+1].size(); ++k) {
-                        mem[i].push_back(path+" "+mem[j+1][k]);
-                    }
+                    for (string str : mem[j+1]) mem[i].push_back(path+" "+str);
                 }
             }
         }
