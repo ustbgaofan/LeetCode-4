d95687fa1a3b4384558984926eaeabae2ce7c6e9
jsc0218 <jsc0218@gmail.com>
2018-03-10 13:51:00 -0500

    Update Longest Substring Without Repeating Characters.h

--- Longest Substring Without Repeating Characters.h
+++ Longest Substring Without Repeating Characters.h
@@ -11,7 +11,23 @@ Given "bbbbb", the answer is "b", with the length of 1.
 Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 */
 
-// Sliding Window + Counting, time O(n), space O(1)
+// Brute force, time O(n^2), space (n)
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int N = s.size(), res = 0;
+        for (int i=0; i<N; ++i) {
+            unordered_set<int> h;
+            for (int j=i; j<N; ++j) {
+                if (!h.insert(s[j]).second) break;
+            }
+            res = max(res, static_cast<int>(h.size()));
+        }
+        return res;
+    }
+};
+
+// Sliding Window, time O(n), space O(1)
 // Q76, Q209
 class Solution {
 public:
@@ -20,7 +36,7 @@ public:
         int res = 0;
         for (int i=0, j=0, cnt=0; j<s.size(); ) {
             if (++m[s[j++]] == 2) ++cnt;
-            while (cnt > 0) {
+            if (cnt > 0) {
                 if (--m[s[i++]] == 1) --cnt;
             }
             res = max(res, j-i);
