1ebf2830ad2054ae99ef7cd62e5a41ff3dcbe919
jsc0218 <jsc0218@gmail.com>
2018-03-10 16:59:29 -0500

    Update Rotate Image.h

--- Rotate Image.h
+++ Rotate Image.h
@@ -4,52 +4,74 @@ You are given an n x n 2D matrix representing an image.
 
 Rotate the image by 90 degrees (clockwise).
 
-Follow up:
-Could you do this in-place?
+Note:
+You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+Example 1:
+
+Given input matrix = 
+[
+  [1,2,3],
+  [4,5,6],
+  [7,8,9]
+],
+
+rotate the input matrix in-place such that it becomes:
+[
+  [7,4,1],
+  [8,5,2],
+  [9,6,3]
+]
+Example 2:
+
+Given input matrix =
+[
+  [ 5, 1, 9,11],
+  [ 2, 4, 8,10],
+  [13, 3, 6, 7],
+  [15,14,12,16]
+], 
+
+rotate the input matrix in-place such that it becomes:
+[
+  [15,13, 2, 5],
+  [14, 3, 4, 1],
+  [12, 6, 8, 9],
+  [16, 7,10,11]
+]
 */
 
 
-// Not In-place Version
+// Non In-place, time O(n^2), space O(n^2)
+// (i,j) --> (j, n-i-1)
 class Solution {
 public:
-    void rotate(vector<vector<int> > &matrix) {
-        vector<vector<int>> tmp(matrix);
+    void rotate(vector<vector<int>>& matrix) {
+        vector<vector<int>> t(matrix);
         int N = matrix.size();
         for (int i=0; i<N; ++i) {
             for (int j=0; j<N; ++j) {
-                matrix[j][N-i-1] = tmp[i][j];
+                matrix[j][N-i-1] = t[i][j];
             }
         }
     }
 };
 
-// In-place Version 1, (i,j) --> (j, n-i-1)
+// In-place, time O(n^2), space O(1)
+/* clockwise rotate
+ * first reverse up to down, then swap the symmetry 
+ * 1 2 3     7 8 9     7 4 1
+ * 4 5 6  => 4 5 6  => 8 5 2
+ * 7 8 9     1 2 3     9 6 3
+*/
 class Solution {
 public:
-    void rotate(vector<vector<int> > &matrix) {
-        int N = matrix.size();
-        for (int i=0; i<N/2; ++i) {
-            for (int j=i; j<N-1-i; ++j) {
-                int t = matrix[i][j];
-                matrix[i][j] = matrix[N-1-j][i];
-                matrix[N-1-j][i] = matrix[N-1-i][N-1-j];
-                matrix[N-1-i][N-1-j] = matrix[j][N-1-i];
-                matrix[j][N-1-i] = t;
-            }
-        }
-    }
-};
-
-// In-place Version 2, --> A^T --> 90C
-class Solution {
-public:
-    void rotate(vector<vector<int> > &matrix) {
-        int N = matrix.size();
-        for (int i=0; i<N; ++i) {
-            for (int j=i+1; j<N; ++j) {
+    void rotate(vector<vector<int>>& matrix) {
+        reverse(matrix.begin(), matrix.end());
+        for (int i=0; i<matrix.size(); ++i) {
+            for (int j=i+1; j<matrix[i].size(); ++j) {
                 swap(matrix[i][j], matrix[j][i]);
             }
-            reverse(matrix[i].begin(), matrix[i].end());
         }
     }
 };
