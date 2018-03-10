5f8afcb949ae1703cd2065a45c1da27e48c2292f
jsc0218 <jsc0218@gmail.com>
2018-03-10 13:27:21 -0500

    Update Add Two Numbers.h

--- Add Two Numbers.h
+++ Add Two Numbers.h
@@ -1,9 +1,14 @@
 // Add Two Numbers
 /*
-You are given two linked lists representing two non-negative numbers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order and each of their nodes contain a single digit. Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example
 
 Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
 Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
 */
 
 /**
@@ -14,21 +19,19 @@ Output: 7 -> 0 -> 8
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
- 
+
+// time O(n), space O(1)
 class Solution {
 public:
     ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
-        ListNode header(0), *cur = &header;
-        int carryBit = 0;
-        while (l1 || l2 || carryBit) {
-            int digit = carryBit;
+        ListNode dummy(0), *cur = &dummy;
+        for (int carry=0; l1 || l2 || carry; ) {
+            int digit = carry;
             if (l1) digit += l1->val, l1 = l1->next;
             if (l2) digit += l2->val, l2 = l2->next;
-            carryBit = digit>=10 ? 1: 0;
-            digit %= 10;
-            cur->next = new ListNode(digit);
-            cur = cur->next;
+            carry = digit/10;
+            cur = cur->next = new ListNode(digit%10);
         }
-        return header.next;
+        return dummy.next;
     }
 };
