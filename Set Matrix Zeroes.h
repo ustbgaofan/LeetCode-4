0694fa6a1f0056e92f5e2e9a53b4172910ca8a6b
jsc0218 <jsc0218@gmail.com>
2018-03-10 21:26:43 -0500

    Update Set Matrix Zeroes.h

--- Set Matrix Zeroes.h
+++ Set Matrix Zeroes.h
@@ -12,93 +12,72 @@ Could you devise a constant space solution?
 */
 
 
-// Version 1, space complexity O(mn)
+// Brute force, time O(mn(m+n)), space O(mn)
 class Solution {
 public:
-    void setZeroes(vector<vector<int> > &matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        vector<vector<pair<int, bool>>> tmpMatrix(m, 
-            vector<pair<int,bool>>(n, pair<int,bool>(0,false)));
-        for (int i=0; i<m; ++i) {
-            for (int j=0; j<n; ++j) {
-                tmpMatrix[i][j].first = matrix[i][j];
-            }
-        }
-        for (int i=0; i<m; ++i) {
-            for (int j=0; j<n; ++j) {
+    void setZeroes(vector<vector<int>>& matrix) {
+        int M = matrix.size(), N = matrix[0].size();
+        vector<vector<bool>> v(M, vector<bool>(N, false));
+        for (int i=0; i<M; ++i) {
+            for (int j=0; j<N; ++j) {
                 if (matrix[i][j] == 0) {
-                    for (int k=0; k<n; ++k) tmpMatrix[i][k].second = true;
-                    for (int k=0; k<m; ++k) tmpMatrix[k][j].second = true;
+                    for (int k=0; k<N; ++k) v[i][k] = true;
+                    for (int k=0; k<M; ++k) v[k][j] = true;
                 }
             }
         }
-        for (int i=0; i<m; ++i) {
-            for (int j=0; j<n; ++j) {
-                if (tmpMatrix[i][j].second) matrix[i][j] = 0;
+        for (int i=0; i<M; ++i) {
+            for (int j=0; j<N; ++j) {
+                if (v[i][j]) matrix[i][j] = 0;
             }
         }
     }
 };
 
-// Version 2, space complexity O(m+n)
+// Row + col, time O(mn), space O(m+n)
 class Solution {
 public:
-    void setZeroes(vector<vector<int> > &matrix) {
-        int m = matrix.size(), n = matrix[0].size();
-        vector<bool> row(m, false), col(n, false);
-        for (int i=0; i<m; ++i) {
-            for (int j=0; j<n; ++j) {
+    void setZeroes(vector<vector<int>>& matrix) {
+        int M = matrix.size(), N = matrix[0].size();
+        vector<bool> row(M, false), col(N, false);
+        for (int i=0; i<M; ++i) {
+            for (int j=0; j<N; ++j) {
                 if (matrix[i][j] == 0) row[i] = col[j] = true;
             }
         }
-        for (int i=0; i<row.size(); ++i) {
-            if (row[i]) {
-                for (int j=0; j<n; ++j) matrix[i][j] = 0;
-            }
-        }
-        for (int j=0; j<col.size(); ++j) {
-            if (col[j]) {
-                for (int i=0; i<m; ++i) matrix[i][j] = 0;
+        for (int i=0; i<M; ++i) {
+            for (int j=0; j<N; ++j) {
+                if (row[i] || col[j]) matrix[i][j] = 0;
             }
         }
     }
 };
  
-// Version 3, store information in first row and first column, without extra space
+// Store information in first row and first column, time O(mn), space O(1)
 class Solution {
 public:
-    void setZeroes(vector<vector<int> > &matrix) {
-        int m = matrix.size(), n = matrix[0].size();
+    void setZeroes(vector<vector<int>>& matrix) {
+        int M = matrix.size(), N = matrix[0].size();
         bool row = false, col = false;
-        for (int i=0; i<n; ++i) {
-            if (matrix[0][i] == 0) {
-                row = true;
-                break;
+        for (int i=0; i<M; ++i) {
+            for (int j=0; j<N; ++j) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) row = true;
+                    if (j == 0) col = true;
+                    matrix[i][0] = matrix[0][j] = 0;
+                }
             }
         }
-        for (int i=0; i<m; ++i) {
-            if (matrix[i][0] == 0) {
-                col = true;
-                break;
-            }
-        }
-        for (int i=1; i<m; ++i) {
-            for (int j=1; j<n; ++j) {
-                if (matrix[i][j] == 0) 
-                    matrix[0][j] = matrix[i][0] = 0;
-            }
-        }
-        for (int i=1; i<m; ++i) {
-            for (int j=1; j<n; ++j) {
-                if (matrix[0][j]==0 || matrix[i][0]==0) 
-                    matrix[i][j] = 0;
+        for (int i=1; i<M; ++i) {
+            for (int j=1; j<N; ++j) {
+                if (matrix[i][0]==0 || matrix[0][j]==0) matrix[i][j] = 0;
             }
         }
         if (row) {
-            for (int i=0; i<n; ++i) matrix[0][i] = 0;
+            for (int i=0; i<N; ++i) matrix[0][i] = 0;
         }
         if (col) {
-            for (int i=0; i<m; ++i) matrix[i][0] = 0;
+            for (int i=0; i<M; ++i) matrix[i][0] = 0;
         }
     }
 };
