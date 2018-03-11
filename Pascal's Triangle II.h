b6b21d20c0a54671e8b3ff00c541b968a7906788
jsc0218 <jsc0218@gmail.com>
2018-03-10 22:17:33 -0500

    Update Pascal's Triangle II.h

--- Pascal's Triangle II.h
+++ Pascal's Triangle II.h
@@ -9,29 +9,17 @@ Note:
 Could you optimize your algorithm to use only O(k) extra space?
 */
 
-// Naive Version, forward, space 2k
+// time O(rowIndex^2), space O(k)
 class Solution {
 public:
-	vector<int> getRow(int rowIndex) {
-		vector<vector<int>> res(2, vector<int>(rowIndex+1));
-		for (int i=0; i<=rowIndex; ++i) {
-			res[i%2][0] = res[i%2][i] = 1;
-			for (int j=1; j<i; ++j)
-				res[i%2][j] = res[(i-1)%2][j] + res[(i-1)%2][j-1];
-		}
-		return res[rowIndex%2];
-	}
-};
-
-// Advanced Version, backward, space k
-class Solution {
-public:
-	vector<int> getRow(int rowIndex) {
-		vector<int> res(rowIndex+1, 0);
-		res[0] = 1;
-		for (int i=0; i<=rowIndex; ++i)
-			for (int j=i; j>=1; --j)
-				res[j] = res[j] + res[j-1];
-		return res;
-	}
+    vector<int> getRow(int rowIndex) {
+        vector<vector<int>> res(2, vector<int>(rowIndex+1));
+        for (int i=0; i<=rowIndex; ++i) {
+            res[i%2][0] = res[i%2][i] = 1;
+            for (int j=1; j<i; ++j) {
+                res[i%2][j] = res[(i-1)%2][j] + res[(i-1)%2][j-1];
+            }
+        }
+        return res[rowIndex%2];
+    }
 };
