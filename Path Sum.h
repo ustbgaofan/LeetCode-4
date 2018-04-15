c89370bb61ff7462ed9f66db6443938dc17bc6f0
jsc0218 <jsc0218@gmail.com>
2018-04-15 19:13:08 -0400

    Update Path Sum.h

--- Path Sum.h
+++ Path Sum.h
@@ -23,6 +23,8 @@ return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+// DFS, time O(n), space O(h)
 class Solution {
 public:
     bool hasPathSum(TreeNode *root, int sum) {
